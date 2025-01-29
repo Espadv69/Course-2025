76a902a7b94ffbc2a01ecc66ee2bc1aac511448f
Espadv69 <aitorjob69@gmail.com>
2025-01-29 12:38:17 +0100

    Fix style href

--- Academy/index.php
+++ Academy/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Academy</title>
-    <link rel="stylesheet" href="../assets/css/global.css">
+    <link rel="stylesheet" href="./assets/css/global.css">
   </head>
   <body>
     <?php require_once "./includes/header.php" ?>
