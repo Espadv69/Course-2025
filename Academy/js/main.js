44454c9a1fdfba07db175a29d93c46201043bd23
Espadv69 <aitorjob69@gmail.com>
2025-01-30 10:36:41 +0100

    Feat: Add function to saved data to localstorage

--- Academy/js/main.js
+++ Academy/js/main.js
@@ -2,4 +2,20 @@ window.addEventListener('DOMContentLoaded', () => {
   // DOM elements
   const $form = document.querySelector('.form-enrollments')
   const $savedDataContainer = document.querySelector('.saved-data')
+
+  // Function to load and display saved enrollments from localstorage
+  function loadSavedData() {
+    const enrollments = JSON.parse(localStorage.getItem('enrollments')) || [] // Initialize as empty array
+    $savedDataContainer.innerHTML = enrollments
+      .map(
+        (data, index) =>
+          `
+        <p>Full Name: <strong>${data.name} ${data.lastname}</strong></p>
+        <p>Degrees: <strong>${data.degree}</strong></p>
+        <button onclick="deleteEnrollments(${index})" class="delete">
+          Delete enrollment
+        </button>
+      `
+      ).join('')
+  }
 })
