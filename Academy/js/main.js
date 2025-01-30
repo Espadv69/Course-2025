58a95182ef6ed08a1bd5d2f59d55eff8d595b697
Espadv69 <aitorjob69@gmail.com>
2025-01-30 10:22:31 +0100

    Feat: Add DOM elements to follow data

---
+++ Academy/js/main.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+  // DOM elements
+  const $form = document.querySelector('.form-enrollments')
+  const $savedDataContainer = document.querySelector('.saved-data')
+})
