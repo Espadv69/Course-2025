07adcaf7d768129cb88bce482bb1c73df96e056a
Espadv69 <aitorjob69@gmail.com>
2025-01-31 11:28:45 +0100

    Add DOM elements

---
+++ Academy/js/assess.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', () => {
+  //DOM elements
+  const $btnCourse1 = document.querySelector('.course1')
+  const $btnCourse2 = document.querySelector('.course2')
+  const $btnCourse3 = document.querySelector('.course3')
+  const $tbody = document.querySelector('.tbody-assess')
+  const $course1_content = (document.querySelector(
+    '.course1-container'
+  ).style.display = 'none')
+  const $course2_content = (document.querySelector(
+    '.course2-container'
+  ).style.display = 'none')
+  const $course3_content = (document.querySelector(
+    '.course3-container'
+  ).style.display = 'none')
+})
