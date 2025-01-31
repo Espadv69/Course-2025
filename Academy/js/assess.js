ab0b67a5cdb1eb4afa9f83163bcf463b4390c504
Espadv69 <aitorjob69@gmail.com>
2025-01-31 11:47:25 +0100

    Feat: Add button functions

--- Academy/js/assess.js
+++ Academy/js/assess.js
@@ -4,13 +4,24 @@ window.addEventListener('DOMContentLoaded', () => {
   const $btnCourse2 = document.querySelector('.course2')
   const $btnCourse3 = document.querySelector('.course3')
   const $tbody = document.querySelector('.tbody-assess')
-  const $course1_content = (document.querySelector(
-    '.course1-container'
-  ).style.display = 'none')
-  const $course2_content = (document.querySelector(
-    '.course2-container'
-  ).style.display = 'none')
-  const $course3_content = (document.querySelector(
-    '.course3-container'
-  ).style.display = 'none')
+  const $course1_content = document.querySelector('.course1-container')
+  const $course2_content = document.querySelector('.course2-container')
+  const $course3_content = document.querySelector('.course3-container')
+
+  // Initialize hidden
+  $course1_content.style.display = 'none'
+  $course2_content.style.display = 'none'
+  $course3_content.style.display = 'none'
+
+  $btnCourse1.addEventListener('click', () => {
+    $course1_content.style.display = 'block'
+  })
+
+  $btnCourse2.addEventListener('click', () => {
+    $course2_content.style.display = 'block'
+  })
+
+  $btnCourse3.addEventListener('click', () => {
+    $course3_content.style.display = 'block'
+  })
 })
