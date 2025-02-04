74450ada099b34c73c56b0009ee01e88fe1689e6
Espadv69 <aitorjob69@gmail.com>
2025-02-04 10:01:37 +0100

    Add router

---
+++ Login-Register/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
