d37f51df524bc289e2ff017cab1d1128c88acdb9
Espadv69 <aitorjob69@gmail.com>
2025-02-04 13:41:20 +0100

    Add hooks to handle screen size and secondary effect to do changes

--- Login-Register/src/components/Navbar.jsx
+++ Login-Register/src/components/Navbar.jsx
@@ -4,6 +4,15 @@ import { FiMenu, Fix } from 'react-icons/fi'
 
 import '../css/Navbar.css'
 
-const Navbar = () => {}
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+  const [isMobile,setIsMobile] = useState(window.innerWidth <= 900)
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 900)
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+}
 
 export default Navbar
