21a3f2be7bd84987ba42db8f358d6cbed2dd5e6e
Espadv69 <aitorjob69@gmail.com>
2025-02-04 10:42:34 +0100

    Add container with title

--- Login-Register/src/components/Login.jsx
+++ Login-Register/src/components/Login.jsx
@@ -3,24 +3,27 @@ import { useNavigate } from 'react-router-dom'
 const Login = () => {
   const navigate = useNavigate()
   return (
-    <form>
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
-      <button>Login</button>
-      <p>
-        If you don't have an account yet{' '}
-        <a
-          href="/register"
-          onClick={(e) => {
-            e.preventDefault()
-            navigate('/register')
-          }}
-        >
-          click here
-        </a>
-      </p>
-    </form>
+    <div className="login_page">
+      <h1 className='h1-title'>Login</h1>
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <button>Login</button>
+        <p>
+          If you don't have an account yet{' '}
+          <a
+            href="/register"
+            onClick={(e) => {
+              e.preventDefault()
+              navigate('/register')
+            }}
+          >
+            click here
+          </a>
+        </p>
+      </form>
+    </div>
   )
 }
 
