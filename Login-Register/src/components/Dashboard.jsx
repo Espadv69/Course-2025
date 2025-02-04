25ef2fc6c32ff50164e4293d5429251fc124e1d7
Espadv69 <aitorjob69@gmail.com>
2025-02-04 12:02:13 +0100

    Add welcome to user and function to handle log out user

--- Login-Register/src/components/Dashboard.jsx
+++ Login-Register/src/components/Dashboard.jsx
@@ -1,6 +1,27 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 const Dashboard = () => {
+  const navigate = useNavigate()
+  const user = localStorage.getItem('loggedInUser')
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/')
+    }
+  }, [user, navigate])
+
+  const handleLogOut = () => {
+    localStorage.removeItem('loggedInUser')
+    navigate('/')
+  }
   return (
-    <h1>Dashboard</h1>
+    <div>
+      <h2>Welcome {user}</h2>
+      <button type="button" onClick={handleLogOut}>
+        Log Out
+      </button>
+    </div>
   )
 }
 
