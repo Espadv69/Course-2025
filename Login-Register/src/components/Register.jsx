d59ff6889fa26a49aa9dd1eddd003e078149c07b
Espadv69 <aitorjob69@gmail.com>
2025-02-04 10:20:22 +0100

    Add necessary imports, Add handleInputs and form structure

--- Login-Register/src/components/Register.jsx
+++ Login-Register/src/components/Register.jsx
@@ -1,3 +1,31 @@
-const Register = () => {}
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+const Register = () => {
+  const [user, setUser] = useState('') // Input user
+  const [email, setEmail] = useState('') // Input email
+  const [pass, setPass] = useState('') // Input pass
+  const navigate = useNavigate()
+
+  return (
+    <form>
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <button>Register</button>
+      <p>
+        If you have an account{' '}
+        <a
+          href="/"
+          onClick={(e) => {
+            e.preventDefault()
+            navigate('/')
+          }}
+        >
+          click here
+        </a>
+      </p>
+    </form>
+  )
+}
 
 export default Register
