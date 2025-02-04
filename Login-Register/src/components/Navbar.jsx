bfb398c2e7d3757fa1ec639b8378ab586289036f
Espadv69 <aitorjob69@gmail.com>
2025-02-04 13:54:11 +0100

    Add navbar component

--- Login-Register/src/components/Navbar.jsx
+++ Login-Register/src/components/Navbar.jsx
@@ -6,13 +6,50 @@ import '../css/Navbar.css'
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false)
-  const [isMobile,setIsMobile] = useState(window.innerWidth <= 900)
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 900)
+
+  const handleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
 
   useEffect(() => {
     const handleResize = () => setIsMobile(window.innerWidth <= 900)
     window.addEventListener('resize', handleResize)
     return () => window.removeEventListener('resize', handleResize)
   }, [])
+
+  return (
+    <nav className="navbar">
+      <div className="logo">React Academy</div>
+
+      {isMobile && (
+        <div className="menu-icon">{menuOpen ? <Fix /> : <FiMenu />}</div>
+      )}
+
+      <ul>
+        <li>
+          <Link to="/dashboard" onClick={handleMenu}>
+            Dashboard
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" onClick={handleMenu}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/services" onClick={handleMenu}>
+            Services
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact" onClick={handleMenu}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
 }
 
 export default Navbar
