045e20dba5502623fb17869701f095a58d29261c
Espadv69 <aitorjob69@gmail.com>
2025-02-04 12:11:29 +0100

    Fix logic to show a logged username

--- Login-Register/src/components/Dashboard.jsx
+++ Login-Register/src/components/Dashboard.jsx
@@ -1,15 +1,18 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const Dashboard = () => {
+  const [user, setUser] = useState(null)
   const navigate = useNavigate()
-  const user = localStorage.getItem('loggedInUser')
 
   useEffect(() => {
-    if (!user) {
+    const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'))
+    if (!loggedInUser) {
       navigate('/')
+    } else {
+      setUser(loggedInUser)
     }
-  }, [user, navigate])
+  }, [navigate])
 
   const handleLogOut = () => {
     localStorage.removeItem('loggedInUser')
@@ -17,7 +20,7 @@ const Dashboard = () => {
   }
   return (
     <div>
-      <h2>Welcome {user}</h2>
+      <h2>Welcome {user?.username}</h2>
       <button type="button" onClick={handleLogOut}>
         Log Out
       </button>
