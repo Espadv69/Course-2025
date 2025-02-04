2b157785a02e9da9c4e36a05971a510b9814c728
Espadv69 <aitorjob69@gmail.com>
2025-02-04 14:01:11 +0100

    Add routes

--- Login-Register/src/App.jsx
+++ Login-Register/src/App.jsx
@@ -1,17 +1,37 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from 'react-router-dom'
 
 import Login from './components/Login'
 import Register from './components/Register'
 import Dashboard from './components/Dashboard'
 
-const App = () => {
+function Layout() {
+  const location = useLocation()
+  const hideNavbarRoutes = ['/', '/register']
+
   return (
-    <Router>
+    <>
+      {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
+    </>
+  )
+}
+
+const App = () => {
+  return (
+    <Router>
+      <Layout />
     </Router>
   )
 }
