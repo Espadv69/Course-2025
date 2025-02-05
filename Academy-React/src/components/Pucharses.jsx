6a8a36d2396ae5b3adccecaf268809450b0a0131
Espadv69 <aitorjob69@gmail.com>
2025-02-05 12:24:41 +0100

    Add title and div container that envolves everthing

--- Academy-React/src/components/Pucharses.jsx
+++ Academy-React/src/components/Pucharses.jsx
@@ -1,18 +1,23 @@
+import '../css/Pucharses.css'
+
 const Pucharses = () => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>Concept</th>
-          <th>Unity</th>
-          <th>Cost</th>
-          <th>Amount</th>
-          <th>Balance</th>
-        </tr>
-      </thead>
-      <tbody className="tbody"></tbody>
-    </table>
+    <div>
+      <h2>Weekly Pucharses</h2>
+      <table className="table_pucharses">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Concept</th>
+            <th>Unity</th>
+            <th>Cost</th>
+            <th>Amount</th>
+            <th>Balance</th>
+          </tr>
+        </thead>
+        <tbody className="tbody"></tbody>
+      </table>
+    </div>
   )
 }
 
