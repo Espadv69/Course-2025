210621456c75166b137383b3ac437453dfa1c925
Espadv69 <aitorjob69@gmail.com>
2025-02-05 19:55:59 +0100

    Add form

--- Academy-React/src/components/Pucharses.jsx
+++ Academy-React/src/components/Pucharses.jsx
@@ -30,6 +30,40 @@ const Pucharses = () => {
   return (
     <div>
       <h2 style={{ textAlign: 'center' }}>Weekly Pucharses Record</h2>
+
+      {/* Form */}
+      <div className="form">
+        <select value={day} onChange={(e) => setDay(e.target.value)}>
+          <option value="">Select Day</option>
+          <option value="Monday">Monday</option>
+          <option value="Tuesday">Tuesday</option>
+          <option value="Wednesday">Wednesday</option>
+          <option value="Thursday">Thursday</option>
+          <option value="Friday">Friday</option>
+          <option value="Saturday">Saturday</option>
+          <option value="Sunday">Sunday</option>
+        </select>
+        <input
+          type="text"
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+          placeholder="Item"
+        />
+        <input
+          type="number"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+          placeholder="Quantity"
+        />
+        <input
+          type="number"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          placeholder="Unit Price"
+        />
+        <button onClick={addRow}>Add</button>
+      </div>
+
       <table className="table_pucharses">
         <thead>
           <tr>
