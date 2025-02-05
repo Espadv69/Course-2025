441bd0458992461af0a1bb8ca0a585ebfda2c4ef
Espadv69 <aitorjob69@gmail.com>
2025-02-05 12:03:27 +0100

    Add thead table

--- Academy-React/src/components/Pucharses.jsx
+++ Academy-React/src/components/Pucharses.jsx
@@ -1,3 +1,15 @@
-const Pucharses = () => {}
+const Pucharses = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>item</th>
+          <th>item</th>
+          <th>item</th>
+        </tr>
+      </thead>
+    </table>
+  )
+}
 
 export default Pucharses
