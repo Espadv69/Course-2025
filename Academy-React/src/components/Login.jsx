b9f9e8f10e329ce3583b4fb249783311cb2dc273
Espadv69 <aitorjob69@gmail.com>
2025-02-05 11:57:25 +0100

    Root folder rename

---
+++ Academy-React/src/components/Login.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import '../css/LoginRegister.css'
+
+const Login = () => {
+  const [identifier, setIdentifier] = useState('') // Can be email or email
+  const [pass, setPass] = useState('')
+  const navigate = useNavigate()
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    const users = JSON.parse(localStorage.getItem('users')) || []
+
+    const user = users.find(
+      (u) =>
+        (u.email === identifier || u.username === identifier) && u.pass === pass
+    )
+
+    if (user) {
+      localStorage.setItem('loggedInUser', JSON.stringify(user))
+      navigate('/dashboard')
+    } else {
+      const $invalid = document.querySelector('.invalid')
+      $invalid.textContent = 'Invalid credentials'
+    }
+  }
+
+  const handleIdentifier = (e) => {
+    const value = e.target.value
+    setIdentifier(value)
+  }
+
+  const handlePassword = (e) => {
+    const value = e.target.value
+    setPass(value)
+  }
+
+  return (
+    <div className="login_page">
+      <h1 className="h1-title">Login</h1>
+      <form className="form_login" onSubmit={handleLogin}>
+        <input
+          type="text"
+          value={identifier}
+          onChange={handleIdentifier}
+          placeholder="Username or email"
+          required
+        />
+        <input
+          type="password"
+          value={pass}
+          onChange={handlePassword}
+          placeholder="Password"
+          required
+        />
+        <button>Login</button>
+        <p style={{ color: 'royalblue' }} className="invalid"></p>
+        <p>
+          If you don't have an account yet{' '}
+          <a
+            href="/register"
+            onClick={(e) => {
+              e.preventDefault()
+              navigate('/register')
+            }}
+          >
+            click here
+          </a>
+        </p>
+      </form>
+    </div>
+  )
+}
+
+export default Login
