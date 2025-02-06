b296a9b53e95ed02a1201d8964a2b15c690da365
Espadv69 <aitorjob69@gmail.com>
2025-02-06 12:42:04 +0100

    Remove unused component import

--- Academy-React/src/App.jsx
+++ Academy-React/src/App.jsx
@@ -12,7 +12,6 @@ import Register from './components/Register'
 import Dashboard from './components/Dashboard'
 import Pucharses from './components/Pucharses'
 import Contact from './components/Contact'
-import About from './components/About'
 
 function Layout() {
   const location = useLocation()
@@ -25,7 +24,6 @@ function Layout() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/about" element={<About />} />
         <Route path="/pucharses" element={<Pucharses />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
