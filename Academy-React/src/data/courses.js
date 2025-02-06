b1364c475940fd1a463a1acc3d9ce0bfe24c1208
Espadv69 <aitorjob69@gmail.com>
2025-02-06 10:05:48 +0100

    Add first course, JavaScript Basics

---
+++ Academy-React/src/data/courses.js
@@ -0,0 +1,9 @@
+const COURSES = [
+  {
+    id: 1,
+    name: 'JavaScript Basics',
+    duration: '4 weeks',
+    description:
+      'Learn the fundamentals of JavaScript, including variables, functions, and loops.',
+  },
+]
