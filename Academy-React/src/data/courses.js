7ce0347898d401f499e8bf476506c770b8105f86
Espadv69 <aitorjob69@gmail.com>
2025-02-06 10:07:28 +0100

    Add second course, React Fundamentals

--- Academy-React/src/data/courses.js
+++ Academy-React/src/data/courses.js
@@ -6,4 +6,11 @@ const COURSES = [
     description:
       'Learn the fundamentals of JavaScript, including variables, functions, and loops.',
   },
+  {
+    id: 2,
+    name: 'React Fundamentals',
+    duration: '6 weeks',
+    description:
+      'Understand the basics of React, including components, state, and props.',
+  },
 ]
