f9695dfb127c0be5d1e58fabcc19cac7ff379d0d
Espadv69 <aitorjob69@gmail.com>
2025-02-06 13:08:05 +0100

    Add temporal list contact

--- Academy-React/src/components/Contact.jsx
+++ Academy-React/src/components/Contact.jsx
@@ -1,3 +1,30 @@
-const Contact = () => {}
+import '../css/Contact.css'
+const Contact = () => {
+  return (
+    <div className="grid-contact">
+      <div>
+        <div>
+          <h3>item</h3>
+          <p>item paragraph</p>
+        </div>
+        <div>
+          <h3>item</h3>
+          <p>item paragraph</p>
+        </div>
+        <div>
+          <h3>item</h3>
+          <p>item paragraph</p>
+        </div>
+      </div>
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <button>Send</button>
+      </form>
+    </div>
+  )
+}
 
 export default Contact
