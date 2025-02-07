a977df5401087267179644e3894d4301e5e98b91
Espadv69 <aitorjob69@gmail.com>
2025-02-07 10:34:06 +0100

    Add Link to Billing page

--- Academy-React/src/components/Navbar.jsx
+++ Academy-React/src/components/Navbar.jsx
@@ -40,6 +40,11 @@ const Navbar = () => {
           </Link>
         </li>
         <li>
+          <Link to="/billing" onClick={() => setMenuOpen(false)}>
+            Billing
+          </Link>
+        </li>
+        <li>
           <Link to="/contact" onClick={() => setMenuOpen(false)}>
             Contact
           </Link>
