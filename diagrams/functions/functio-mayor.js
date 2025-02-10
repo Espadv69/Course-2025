51282529281e756991e655f65b8865147e57fa42
Espadv69 <aitorjob69@gmail.com>
2025-02-10 13:36:21 +0100

    Rename

--- diagrams/functions/functio-mayor.js
+++
@@ -1,7 +0,0 @@
-function mayor(num1 = Number, num2 = Number) {
-  if (num1 === num2) return console.log('No pueden ser dos números iguales')
-  if (num1 > num2) return console.log(num1)
-  return console.log(num2)
-}
-
-mayor(7, 4)
