5d9f60d245bafc5335abbba091a00b140165a2ce
Espadv69 <aitorjob69@gmail.com>
2025-02-12 11:32:33 +0100

    Add necessary hooks

--- HandleProducts/src/pages/Home.jsx
+++ HandleProducts/src/pages/Home.jsx
@@ -1,3 +1,8 @@
 import { useState } from 'react'
 import { useAuth } from '../auth/authContext'
 import { productDatas } from '../data/data'
+
+const ProductsList = () => {
+  const [products, setProducts] = useState(productDatas)
+  const { user } = useAuth()
+}
