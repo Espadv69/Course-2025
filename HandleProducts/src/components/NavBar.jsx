f3ac199b1c631d7be2504fe0f3f9ce8ca2d5368e
Espadv69 <aitorjob69@gmail.com>
2025-02-12 11:42:43 +0100

    Add NavBar structure dinamic

--- HandleProducts/src/components/NavBar.jsx
+++ HandleProducts/src/components/NavBar.jsx
@@ -1,2 +1,20 @@
 import { Link } from 'react-router-dom'
 import { useAuth } from '../auth/authContext'
+
+const NavBar = () => {
+  const { user, logout } = useAuth()
+
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      {user && <button onClick={logout}>Logout</button>}
+      {user && user.role === 'admin' && (
+        <div>
+          <Link to="/add-product">Add Product</Link>
+        </div>
+      )}
+    </nav>
+  )
+}
+
+export default NavBar
