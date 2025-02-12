6f46353fbf8e7e069e870463d14743a9e360be3f
Espadv69 <aitorjob69@gmail.com>
2025-02-12 12:02:57 +0100

    Add App component to navigate between pages and WrappedApp to envolve App component with authentication Provider

--- HandleProducts/src/App.jsx
+++ HandleProducts/src/App.jsx
@@ -4,3 +4,28 @@ import NavBar from './components/NavBar'
 import ProductList from './pages/Home'
 import AddProduct from './pages/AddProduct'
 import Login from './auth/Login'
+
+const App = () => {
+  const { user } = useAuth()
+
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/login" element={<Login />} />
+        {user && user.role === 'admin' && (
+          <Route path="/add-product" element={<AddProduct />} />
+        )}
+      </Routes>
+    </Router>
+  )
+}
+
+const WrappedApp = () => {
+  ;<AuthProvider>
+    <App />
+  </AuthProvider>
+}
+
+export default WrappedApp
