9f82a8c334fd1a04edd35d9f69d5bd8fedc66939
Espadv69 <aitorjob69@gmail.com>
2025-02-12 11:00:41 +0100

    Add main hooks

--- HandleProducts/src/auth/Login.jsx
+++ HandleProducts/src/auth/Login.jsx
@@ -1,3 +1,9 @@
 import { useState } from 'react'
 import { useAuth } from './authContext'
 import { useNavigate } from 'react-router-dom'
+
+const Login = () => {
+  const [username, setUsername] = useState('')
+  const { login } = useAuth()
+  const navigate = useNavigate()
+}
