c9ffd86aa56b15706ef0d02ff8a7bca1291d8b66
Espadv69 <aitorjob69@gmail.com>
2025-02-21 10:18:18 +0100

    Refactor theme toggle functionality and initialize DOM elements on page load

--- Kitchen/main.js
+++ Kitchen/main.js
@@ -1,10 +1,13 @@
-// DOM elements
-const $themeBtn = document.querySelector('.themeToogle')
-
-
 // Toogle Theme
+const $themeBtn = document.querySelector('.themeToogle')
 let isDark = false
 $themeBtn.addEventListener('click', () => {
   isDark = !isDark
   document.body.className = isDark ? 'dark' : 'light'
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  // DOM
+  const $tbody = document.querySelector('tbody')
+  const $addProductBtn = document.querySelector('.addProduct')
 })
