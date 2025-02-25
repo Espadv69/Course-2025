8f08cdb13b59c558416cdb3705926b829b5923dd
Espadv69 <aitorjob69@gmail.com>
2025-02-25 11:50:25 +0100

    Add hooks to handle new questions and hook to get questions from localstorage

--- Quiz-App/src/pages/AddQuestions.jsx
+++ Quiz-App/src/pages/AddQuestions.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import { saveQuestions, getQuestions } from '../utils/localStorage'
 
-const AddQuestions = () => {}
+const AddQuestions = () => {
+  const [question, setQuestion] = useState('') // Hanlde input question
+  const [questions, setQuestions] = useState(getQuestions()) // Get questions from local storage
+}
 
 export default AddQuestions
