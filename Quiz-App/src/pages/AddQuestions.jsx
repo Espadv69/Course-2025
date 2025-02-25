933df3f6d00809e3b98a51a07d07040ff7c3b323
Espadv69 <aitorjob69@gmail.com>
2025-02-25 11:55:11 +0100

    Add functionality to handle adding new questions and saving to local storage

--- Quiz-App/src/pages/AddQuestions.jsx
+++ Quiz-App/src/pages/AddQuestions.jsx
@@ -4,6 +4,15 @@ import { saveQuestions, getQuestions } from '../utils/localStorage'
 const AddQuestions = () => {
   const [question, setQuestion] = useState('') // Hanlde input question
   const [questions, setQuestions] = useState(getQuestions()) // Get questions from local storage
+
+  const handleAddQuestion = () => {
+    if (!question.trim()) return
+
+    const newQuestions = [...questions, { text: question }]
+    setQuestions(newQuestions)
+    saveQuestions(newQuestions)
+    setQuestion('')
+  }
 }
 
 export default AddQuestions
