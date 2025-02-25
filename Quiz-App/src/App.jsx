79f403cb535abbab9905428775a10e30eadd9286
Espadv69 <aitorjob69@gmail.com>
2025-02-25 10:35:08 +0100

    Initialize Exam structure

--- Quiz-App/src/App.jsx
+++ Quiz-App/src/App.jsx
@@ -1,3 +1,10 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import AddQuestions from './pages/AddQuestions'
+import GenerateQuestions from './pages/GenerateQuestions'
+import TakeExam from './pages/TakeExam'
+
 const App = () => {}
 
 export default App
