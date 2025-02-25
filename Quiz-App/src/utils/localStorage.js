4dfd4d41135d5e430654c8dd93bc5ff0a2124274
Espadv69 <aitorjob69@gmail.com>
2025-02-25 10:31:56 +0100

    Add localStorage utility functions for questions and answers

---
+++ Quiz-App/src/utils/localStorage.js
@@ -0,0 +1,15 @@
+export const saveQuestion = (questions) => {
+  localStorage.setItem('questions', JSON.stringify(questions))
+}
+
+export const getQuestions = () => {
+  return JSON.parse(localStorage.getItem('questions')) || []
+}
+
+export const saveAnswers = (answers) => {
+  localStorage.setItem('answers', JSON.stringify(answers))
+}
+
+export const getAnswers = () => {
+  return JSON.parse(localStorage.getItem('answers')) || []
+}
