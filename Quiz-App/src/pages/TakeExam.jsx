34bec7382a279e9cb97a23ffde4c9bfd64af651f
Espadv69 <aitorjob69@gmail.com>
2025-02-25 12:11:56 +0100

    Add state management and question retrieval in TakeExam component

--- Quiz-App/src/pages/TakeExam.jsx
+++ Quiz-App/src/pages/TakeExam.jsx
@@ -1,3 +1,9 @@
-const TakeExam = () => {}
+import { useState } from 'react'
+import { saveQuestions, getQuestions } from '../utils/localStorage.js'
+
+const TakeExam = () => {
+  const questions = getQuestions()
+  const [answers, setAnswers] = useState({})
+}
 
 export default TakeExam
