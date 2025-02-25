0a43214d9baf95c44950518615ef7b1f8c0d2130
Espadv69 <aitorjob69@gmail.com>
2025-02-25 10:42:55 +0100

    Implement navigation links in NavBar component

--- Quiz-App/src/components/NavBar.jsx
+++ Quiz-App/src/components/NavBar.jsx
@@ -1,3 +1,22 @@
-const NavBar = () => {}
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <nav className="nav">
+      <Link to="/" className="nav-item">
+        Home
+      </Link>
+      <Link to="/add" className="nav-item">
+        Add Questions
+      </Link>
+      <Link to="/generate" className="nav-item">
+        Generate Questions
+      </Link>
+      <Link to="/exam" className="nav-item">
+        Take Exam
+      </Link>
+    </nav>
+  )
+}
 
 export default NavBar
