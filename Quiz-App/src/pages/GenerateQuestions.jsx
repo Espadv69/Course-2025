f7302d9e4385c3f0a8106828c7b4f2b829d49347
Espadv69 <aitorjob69@gmail.com>
2025-02-26 09:36:47 +0100

    Add predefined questions and import necessary utilities in GenerateQuestions component

--- Quiz-App/src/pages/GenerateQuestions.jsx
+++ Quiz-App/src/pages/GenerateQuestions.jsx
@@ -1,3 +1,14 @@
+import { useState } from 'react'
+import { saveQuestions, getQuestions } from '../utils/localStorage.js'
+
+const predefinedQuestions = [
+  'What it the capital of France?',
+  'Who wrote "To Kill a Mockingbird"?',
+  'What is 5 + 7?',
+  'Name a programming language that starts with "P".',
+  'What year did the first man land on the moon?',
+]
+
 const GenerateQuestions = () => {}
 
 export default GenerateQuestions
