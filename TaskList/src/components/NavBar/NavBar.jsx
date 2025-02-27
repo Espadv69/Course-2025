9b434f120781caa71d093109c5ead07260f5008b
Espadv69 <aitorjob69@gmail.com>
2025-02-27 12:08:21 +0100

    Add navigation links to NavBar component

--- TaskList/src/components/NavBar/NavBar.jsx
+++ TaskList/src/components/NavBar/NavBar.jsx
@@ -1,3 +1,12 @@
-const NavBar = () => {}
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <nav className="nav">
+      <Link to="/">Task List</Link>
+      <Link to="/form">Task Form</Link>
+    </nav>
+  )
+}
 
 export default NavBar
