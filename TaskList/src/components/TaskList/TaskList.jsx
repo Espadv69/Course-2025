c5f4ee041dfe7cac4fd33af4f8ddc474e7b8f6a3
Espadv69 <aitorjob69@gmail.com>
2025-02-27 13:33:06 +0100

    Improve TaskList component styling for empty state and task list

--- TaskList/src/components/TaskList/TaskList.jsx
+++ TaskList/src/components/TaskList/TaskList.jsx
@@ -2,10 +2,11 @@ import TaskItem from '../TaskItem/TaskItem'
 import './TaskList.css'
 
 const TaskList = ({ tasks, ondeleteTask }) => {
-  if (tasks.length === 0) return <p>No taks found!</p>
+  if (tasks.length === 0)
+    return <p style={{ margin: '40px auto' }}>No taks found!</p>
 
   return (
-    <ul>
+    <ul style={{ marginTop: '40px' }}>
       {tasks.map((task, index) => (
         <TaskItem
           key={index}
