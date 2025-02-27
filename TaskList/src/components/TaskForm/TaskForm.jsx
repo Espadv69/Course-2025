4fef6bce124fc90f568b54f871652a8b4dfa917a
Espadv69 <aitorjob69@gmail.com>
2025-02-27 12:56:01 +0100

    Add h2 with TaskForm title and add classname to dom elements

--- TaskList/src/components/TaskForm/TaskForm.jsx
+++ TaskList/src/components/TaskForm/TaskForm.jsx
@@ -5,11 +5,13 @@ const TaskForm = ({ onAddTask }) => {
   const [taskName, setTaskName] = useState('')
 
   return (
-    <form style={{ marginTop: '40px' }}>
-      <label>
+    <form style={{ marginTop: '40px' }} className="task__form">
+      <h2>Task Form</h2>
+      <label className="task__label">
         Task Name:
         <input
           type="text"
+          className="task__input"
           value={taskName}
           onChange={(e) => setTaskName(e.target.value)}
           placeholder="Enter the TaskName"
