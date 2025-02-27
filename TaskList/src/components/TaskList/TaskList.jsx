76bdeb1bea23ef9ea53a1d54b827fb8b183b50b9
Espadv69 <aitorjob69@gmail.com>
2025-02-27 13:20:20 +0100

    Implement TaskList component to display tasks and handle deletion

--- TaskList/src/components/TaskList/TaskList.jsx
+++ TaskList/src/components/TaskList/TaskList.jsx
@@ -1,6 +1,20 @@
 import TaskItem from '../TaskItem/TaskItem'
 import './TaskList.css'
 
-const TaskList = () => {}
+const TaskList = ({ tasks, ondeleteTask }) => {
+  if (tasks.lenght === 0) return <p>No taks found!</p>
+
+  return (
+    <ul>
+      {tasks.map((task, index) => (
+        <TaskItem
+          key={index}
+          task={task}
+          ondeleteTask={() => ondeleteTask(index)}
+        />
+      ))}
+    </ul>
+  )
+}
 
 export default TaskList
