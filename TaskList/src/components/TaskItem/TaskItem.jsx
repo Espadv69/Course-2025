f1d7b0522788d9ccd261c203761f92e9d2527994
Espadv69 <aitorjob69@gmail.com>
2025-02-27 13:15:25 +0100

    Implement TaskItem component with delete functionality and styling

--- TaskList/src/components/TaskItem/TaskItem.jsx
+++ TaskList/src/components/TaskItem/TaskItem.jsx
@@ -1,3 +1,12 @@
-const TaskItem = ({ task, ondeleteTask }) => {}
+import './TaskItem.css'
+
+const TaskItem = ({ task, ondeleteTask }) => {
+  return (
+    <li>
+      <p>{task.task}</p>
+      <button onClick={ondeleteTask}>Delete</button>
+    </li>
+  )
+}
 
 export default TaskItem
