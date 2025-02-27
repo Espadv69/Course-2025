886c5c5aab230baf9a20b2f396a5b179ca87134c
Espadv69 <aitorjob69@gmail.com>
2025-02-27 12:05:43 +0100

    Implement routing with react-router-dom and add NavBar component

--- TaskList/src/App.jsx
+++ TaskList/src/App.jsx
@@ -1,3 +1,15 @@
-const App = () => {}
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar/NavBar'
+
+const App = () => {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </Router>
+  )
+}
 
 export default App
