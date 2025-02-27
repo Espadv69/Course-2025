e4d7b5ae1a59e9529777e47848d7602f79567b3c
Espadv69 <aitorjob69@gmail.com>
2025-02-27 12:11:17 +0100

    Add routing for TaskList and TaskForm components

--- TaskList/src/App.jsx
+++ TaskList/src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar'
 
+import TaskForm from './components/TaskForm/TaskForm'
+import TaskList from './components/TaskList/TaskList'
+
 const App = () => {
   return (
     <Router>
       <NavBar />
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<TaskList />} />
+        <Route path="/form" element={<TaskForm />} />
       </Routes>
     </Router>
   )
