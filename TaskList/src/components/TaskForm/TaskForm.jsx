7ff3b18ca5924f633f2254c26a6538af076a75e1
Espadv69 <aitorjob69@gmail.com>
2025-02-27 12:25:55 +0100

    Add input field for task name in TaskForm component

--- TaskList/src/components/TaskForm/TaskForm.jsx
+++ TaskList/src/components/TaskForm/TaskForm.jsx
@@ -2,6 +2,20 @@ import { useState } from 'react'
 
 const TaskForm = ({ onAddTask }) => {
   const [taskName, setTaskName] = useState('')
+
+  return (
+    <form>
+      <label>
+        Task Name:
+        <input
+          type="text"
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
+          placeholder="Enter the TaskName"
+        />
+      </label>
+    </form>
+  )
 }
 
 export default TaskForm
