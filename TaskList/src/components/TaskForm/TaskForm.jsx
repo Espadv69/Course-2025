eb66c477c322280142a76a5056adc04f56572287
Espadv69 <aitorjob69@gmail.com>
2025-02-27 13:00:15 +0100

    Refactor TaskForm component to streamline form submission and reset task name after adding

--- TaskList/src/components/TaskForm/TaskForm.jsx
+++ TaskList/src/components/TaskForm/TaskForm.jsx
@@ -9,14 +9,13 @@ const TaskForm = ({ onAddTask }) => {
 
     if (taskName.trim() === '')
       return alert('You should fill out the TaskName field')
+
+    onAddTask(taskName)
+    setTaskName('')
   }
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      style={{ marginTop: '40px' }}
-      className="task__form"
-    >
+    <form onSubmit={handleSubmit} className="task__form">
       <h2>Task Form</h2>
       <label className="task__label">
         Task Name:
