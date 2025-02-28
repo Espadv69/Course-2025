ae7cfe5647bc816f35a15e4d50a2431dc6f39911
Espadv69 <aitorjob69@gmail.com>
2025-02-28 12:02:02 +0100

    Implement timer functionality in Stopwatch component

--- Stopwatch/src/components/Stopwatch.jsx
+++ Stopwatch/src/components/Stopwatch.jsx
@@ -3,6 +3,18 @@ import { useState, useEffect } from 'react'
 const Stopwatch = () => {
   const [time, setTime] = useState(0)
   const [isRunning, setIsRunning] = useState(false)
+
+  useEffect(() => {
+    let interval
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTime((prev) => prev + 1)
+      }, 1000)
+    } else {
+      clearInterval(interval)
+    }
+    return () => clearInterval(interval)
+  }, [isRunning])
 }
 
 export default Stopwatch
