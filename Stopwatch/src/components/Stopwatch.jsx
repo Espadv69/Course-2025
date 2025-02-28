ff558523e4808fea613e237c1f41b942f9d948c3
Espadv69 <aitorjob69@gmail.com>
2025-02-28 11:59:46 +0100

    Add state management for Stopwatch component

--- Stopwatch/src/components/Stopwatch.jsx
+++ Stopwatch/src/components/Stopwatch.jsx
@@ -1,3 +1,8 @@
-const Stopwatch = () => {}
+import { useState, useEffect } from 'react'
+
+const Stopwatch = () => {
+  const [time, setTime] = useState(0)
+  const [isRunning, setIsRunning] = useState(false)
+}
 
 export default Stopwatch
