b1350d473b9dbfa040e9b43604ec7a5939beb697
Espadv69 <aitorjob69@gmail.com>
2025-02-28 12:47:30 +0100

    Add additional quote to QuoteGenerator component

--- QuoteGenerator/src/components/QuoteGenerator.jsx
+++ QuoteGenerator/src/components/QuoteGenerator.jsx
@@ -2,8 +2,12 @@ import { useState } from 'react'
 
 const QUOTES = [
   {
-    text: 'The only way to do great work is to love what you do',
+    text: 'The only way to do great work is to love what you do.',
     author: 'Steve Jobs',
+  },
+  {
+    text: "Life is what happens when you're busy making other plans.",
+    author: 'John Lennon',
   },
 ]
 
