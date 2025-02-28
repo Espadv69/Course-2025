8bbbee213d52e0248b6867f1c8d84ba32b82d0df
Espadv69 <aitorjob69@gmail.com>
2025-02-28 12:46:04 +0100

    Add initial quote data to QuoteGenerator component

--- QuoteGenerator/src/components/QuoteGenerator.jsx
+++ QuoteGenerator/src/components/QuoteGenerator.jsx
@@ -1,3 +1,12 @@
+import { useState } from 'react'
+
+const QUOTES = [
+  {
+    text: 'The only way to do great work is to love what you do',
+    author: 'Steve Jobs',
+  },
+]
+
 const QuoteGenerator = () => {}
 
 export default QuoteGenerator
