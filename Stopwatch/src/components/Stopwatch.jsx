9efd4dff5e6fd24709067f7a04420f92f2a812a2
Espadv69 <aitorjob69@gmail.com>
2025-02-28 12:06:16 +0100

    Add time formatting function to Stopwatch component

--- Stopwatch/src/components/Stopwatch.jsx
+++ Stopwatch/src/components/Stopwatch.jsx
@@ -15,6 +15,12 @@ const Stopwatch = () => {
     }
     return () => clearInterval(interval)
   }, [isRunning])
+
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60)
+    const secs = seconds % 60
+    return `${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`
+  }
 }
 
 export default Stopwatch
