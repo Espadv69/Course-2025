7a77cd5385bb582e91a74d97cb31a6131718b0c0
Espadv69 <aitorjob69@gmail.com>
2025-03-03 10:43:36 +0100

    Add Game import and render it

--- rock-paper-scissors/src/App.jsx
+++ rock-paper-scissors/src/App.jsx
@@ -1,11 +1,7 @@
+import Game from './components/Game'
+
 const App = () => {
-  return (
-    <div className="bg-blue-500 w-full py-2">
-      <h1 className="text-white text-center sm:text-5xl text-2xl font-bold">
-        Rock, Paper and Scissors
-      </h1>
-    </div>
-  )
+  return <Game />
 }
 
 export default App
