13e0bb9e979daa23f25c8b723331f94f63929dd2
Espadv69 <aitorjob69@gmail.com>
2025-03-03 13:18:42 +0100

    Refactor using "tailwind" in js

--- testTailwind/main.js
+++ testTailwind/main.js
@@ -3,9 +3,10 @@ const $addContainerBtn = document.querySelector('#add')
 
 $addContainerBtn.addEventListener('click', () => {
   const $divContainer = document.createElement('div')
-  $divContainer.style.width = '200px'
-  $divContainer.style.height = '200px'
-  $divContainer.style.backgroundColor = '#0066ff'
+  $divContainer.classList.add('bg-blue-600', 'size-[300px]')
+  // $divContainer.style.width = '200px'
+  // $divContainer.style.height = '200px'
+  // $divContainer.style.backgroundColor = '#0066ff'
 
   $containers.appendChild($divContainer)
   console.log('container create')
