26d9e80537d15b2ac48da5f0ddcb7f45fd69b608
Espadv69 <aitorjob69@gmail.com>
2025-03-03 11:27:49 +0100

    Enhance ChoiceButton styling with updated text sizes for better visibility

--- rock-paper-scissors/src/components/ChoiceButton.jsx
+++ rock-paper-scissors/src/components/ChoiceButton.jsx
@@ -1,7 +1,7 @@
 const ChoiceButton = ({ choice, onClick }) => {
   return (
     <button
-      className="cursor-pointer bg-blue-600 text-white border-gray-100 rounded p-3.5 lg:w-48 md:w-32 w-full"
+      className="cursor-pointer bg-blue-600 text-white border-gray-100 rounded p-3.5 lg:w-48 md:w-32 w-full lg:text-4xl md:text-3xl text-xl"
       onClick={() => onClick(choice)}
     >
       {choice}
