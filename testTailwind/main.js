41526efe91347c27d87de472baf0e44a8c455cc4
Espadv69 <aitorjob69@gmail.com>
2025-03-03 13:13:35 +0100

    Add event listener to create and append a new container on button click

--- testTailwind/main.js
+++ testTailwind/main.js
@@ -1,1 +1,12 @@
+const $containers = document.querySelector('#containers')
 const $addContainerBtn = document.querySelector('#add')
+
+$addContainerBtn.addEventListener('click', () => {
+  const $divContainer = document.createElement('div')
+  $divContainer.style.width = '200px'
+  $divContainer.style.height = '200px'
+  $divContainer.style.backgroundColor = '#0066ff'
+
+  $containers.appendChild($divContainer)
+  console.log('container create')
+})
