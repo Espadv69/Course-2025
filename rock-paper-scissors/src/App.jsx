1edf626f217e54ede2469a058a770ee3af76e5c9
Espadv69 <aitorjob69@gmail.com>
2025-03-03 10:21:39 +0100

    Add header and background styling to App component

--- rock-paper-scissors/src/App.jsx
+++ rock-paper-scissors/src/App.jsx
@@ -1,3 +1,11 @@
-const App = () => {}
+const App = () => {
+  return (
+    <div className="bg-blue-500 w-full py-2">
+      <h1 className="text-white text-center sm:text-5xl text-2xl font-bold">
+        Rock, Paper and Scissors
+      </h1>
+    </div>
+  )
+}
 
 export default App
