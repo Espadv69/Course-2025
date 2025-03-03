0b200174ca631051c5b4f92d74709a1af178232a
Espadv69 <aitorjob69@gmail.com>
2025-03-03 10:48:01 +0100

    Enhance ChoiceButton styling with improved button design and cursor pointer

--- rock-paper-scissors/src/components/ChoiceButton.jsx
+++ rock-paper-scissors/src/components/ChoiceButton.jsx
@@ -1,5 +1,12 @@
 const ChoiceButton = ({ choice, onClick }) => {
-  return <button onClick={() => onClick(choice)}>{choice}</button>
+  return (
+    <button
+      className="cursor-pointer bg-blue-600 text-white border-gray-100 rounded p-3.5"
+      onClick={() => onClick(choice)}
+    >
+      {choice}
+    </button>
+  )
 }
 
 export default ChoiceButton
