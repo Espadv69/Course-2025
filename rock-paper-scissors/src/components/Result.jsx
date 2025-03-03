f2d9e2827b03e8aa5c387726bf417e4f247b0242
Espadv69 <aitorjob69@gmail.com>
2025-03-03 12:07:09 +0100

    Enhance Result component styling with improved layout and responsive design

--- rock-paper-scissors/src/components/Result.jsx
+++ rock-paper-scissors/src/components/Result.jsx
@@ -1,6 +1,6 @@
 const Result = ({ userChoice, computerChoice, result }) => {
   return (
-    <div className="">
+    <div className="py-8 px-2 flex flex-col gap-4 mt-4 bg-blue-400 text-white lg:text-4xl md:text-3xl text-2xl m-auto max-w-2xl">
       <h2>Your choice: {userChoice}</h2>
       <h2>Computer choice: {computerChoice}</h2>
       <h2>{result}</h2>
