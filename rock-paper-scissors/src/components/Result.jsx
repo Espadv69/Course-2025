0e39d257e35b6cbadada9b6e4c5b323f6e480c63
Espadv69 <aitorjob69@gmail.com>
2025-03-03 10:28:58 +0100

    Update Result component to display user choice, computer choice, and result

--- rock-paper-scissors/src/components/Result.jsx
+++ rock-paper-scissors/src/components/Result.jsx
@@ -1,3 +1,11 @@
-const Result = () => {}
+const Result = ({ userChoice, computerChoice, result }) => {
+  return (
+    <div className="">
+      <h2>Your choice: {userChoice}</h2>
+      <h2>Computer choice: {computerChoice}</h2>
+      <h2>{result}</h2>
+    </div>
+  )
+}
 
 export default Result
