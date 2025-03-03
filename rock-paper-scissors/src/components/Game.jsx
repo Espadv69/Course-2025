9ceec947414848fe93cc50583ee47d77cf9628a1
Espadv69 <aitorjob69@gmail.com>
2025-03-03 10:31:47 +0100

    Add CHOICES constant

--- rock-paper-scissors/src/components/Game.jsx
+++ rock-paper-scissors/src/components/Game.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import ChoiceButton from './ChoiceButton'
 import Result from './Result'
 
+const CHOICES = ['Rock', 'Paper', 'Scissors']
+
 const Game = () => {}
 
 export default Game
