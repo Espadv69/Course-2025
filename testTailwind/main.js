a17555a43df5938cb295954c11915feb2e849363
Espadv69 <aitorjob69@gmail.com>
2025-03-04 09:02:16 +0100

    Remove inline styles from container creation and use Tailwind classes instead

--- testTailwind/main.js
+++ testTailwind/main.js
@@ -4,10 +4,7 @@ const $addContainerBtn = document.querySelector('#add')
 $addContainerBtn.addEventListener('click', () => {
   const $divContainer = document.createElement('div')
   $divContainer.classList.add('bg-blue-600', 'size-[300px]')
-  // $divContainer.style.width = '200px'
-  // $divContainer.style.height = '200px'
-  // $divContainer.style.backgroundColor = '#0066ff'
-
+  
   $containers.appendChild($divContainer)
   console.log('container create')
 })
