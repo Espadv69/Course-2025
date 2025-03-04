615e9fdc3d905821a6f6af21c0861c03b7223b28
Espadv69 <aitorjob69@gmail.com>
2025-03-04 09:18:30 +0100

    Add card initialization and shuffling logic to MemoryGame component

--- memory/src/components/MemoryGame.jsx
+++ memory/src/components/MemoryGame.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from 'react'
 
+const initialCards = ['🍎', '🍌', '🍒', '🍓', '🍑', '🍍', '🥭', '🥝']
+const shuffleCards = [...initialCards, ...initialCards].sort(
+  () => Math.random() - 0.5
+)
+
 const MemoryGame = () => {}
 
 export default MemoryGame
