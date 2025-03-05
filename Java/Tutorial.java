34b49b8f64aa3f24bce7cd670287d7dd3651948f
Espadv69 <aitorjob69@gmail.com>
2025-03-05 10:24:05 +0100

    Add enhanced for loop example to Java tutorial

--- Java/Tutorial.java
+++ Java/Tutorial.java
@@ -24,6 +24,10 @@ public class Tutorial {
     for (int i = 0; i < numbers.length; i++) {
       System.out.println("numbers[" + i + "] = " + numbers[i]);
     }
+
+    for (int number : numbers) {
+      System.out.println("number = " + number);
+    }
   }
 
 }
