10276f2d41f672c46c39a0dddcca2ca76db5f2e0
Espadv69 <aitorjob69@gmail.com>
2025-03-05 09:36:50 +0100

    Add initial Java tutorial with Hello World example and counting loop

---
+++ Java/Tutorial.java
@@ -0,0 +1,13 @@
+public class Tutorial {
+
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    int x = 1;
+    int y = 10;
+    while (x <= y) {
+      System.out.println(x);
+      x++;
+    }
+  }
+
+}
