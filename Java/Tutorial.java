420d615987d9beccd9334999a18faffbc8a68f3e
Espadv69 <aitorjob69@gmail.com>
2025-03-05 10:19:05 +0100

    Add for loop example to Java tutorial

--- Java/Tutorial.java
+++ Java/Tutorial.java
@@ -19,6 +19,11 @@ public class Tutorial {
       System.out.println("do while x = " + x);
       x++;
     } while (x <= y);
+
+    int[] numbers = {1, 2, 3, 4, 5};
+    for (int i = 0; i < numbers.length; i++) {
+      System.out.println("numbers[" + i + "] = " + numbers[i]);
+    }
   }
 
 }
