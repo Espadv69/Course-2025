fd7f23d9fe63e4f945aa47d5d4f7bd855033530f
Espadv69 <aitorjob69@gmail.com>
2025-03-05 14:34:11 +0100

    Add Tienda.java to calculate total price based on product price and quantity

---
+++ Java/Tienda.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Tienda {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Add the price of the product");
+    double price = sc.nextDouble();
+
+    System.out.println("Add the quantity of the product");
+    int quantity = sc.nextInt();
+
+    double total = price * quantity;
+    System.out.println("The total is: " + total);
+
+    sc.close();
+  }
+}
