69e15e56de079a0a2393fc509f60ee4897123174
Espadv69 <aitorjob69@gmail.com>
2025-03-05 14:07:31 +0100

    Add Exercise_03.java for basic arithmetic operations

---
+++ Java/Exercise_03.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercise_03 {
+
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Add first value");
+		int num1 = sc.nextInt();
+
+		System.out.println("Add second value");
+		int num2 = sc.nextInt();
+
+		System.out.println("First value: " + num1 + " Second value: " + num2);
+
+		int sum = num1 + num2;
+		int multiply = num1 * num2;
+		System.out.println("Sum: " + sum);
+		System.out.println("Multiply: " + multiply);
+	}
+
+}
