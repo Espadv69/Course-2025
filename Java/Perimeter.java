ec1ac1dfbf9b185d85eb72636a60ec9874da5533
Espadv69 <aitorjob69@gmail.com>
2025-03-05 14:24:01 +0100

    Add Perimeter.java to calculate the perimeter of a square

---
+++ Java/Perimeter.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Perimeter {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Add side size");
+		int side = sc.nextInt();
+
+		int perimeter = side * 4;
+		System.out.println("The perimeter is: " + perimeter);
+
+		sc.close();
+	}
+
+}
