e1ff42f62b9b1c60bca8bdc0afac0f7c04304e71
Espadv69 <aitorjob69@gmail.com>
2025-03-06 12:48:44 +0100

    Add salary input and tax assessment logic in SueldoIngresar main method

--- Java/SueldoIngresar.java
+++ Java/SueldoIngresar.java
@@ -4,6 +4,16 @@ public class SueldoIngresar {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+
+    System.out.println("Ingrese sueldo: ");
+    double sueldo = sc.nextDouble();
+
+    if (sueldo > 3000) {
+      System.out.println("Debe pagar impuestos");
+    } else {
+      System.out.println("No debe pagar impuestos");
+    }
+
     sc.close();
   }
 
