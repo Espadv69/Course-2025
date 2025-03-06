6f918c690b979cb2ccd52e892d20f8adede7dc12
Espadv69 <aitorjob69@gmail.com>
2025-03-06 11:34:58 +0100

    Add Dialog_input.java for user input via dialog boxes

---
+++ Java/Dialog_input.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class Dialog_input {
+
+	public static void main(String[] args) {
+		
+		String nombreUsuario = JOptionPane.showInputDialog("Nombre");
+		String edad = JOptionPane.showInputDialog("Edad");
+		System.out.print("Hola, " + nombreUsuario + ". Tienes " + edad + " años.");
+
+	}
+
+}
