1baf1ea1b10c69865cd1e8117c2340b66576a1a5
Espadv69 <aitorjob69@gmail.com>
2025-03-06 13:55:18 +0100

    Change integer variables to float in OperacionDos for improved precision in arithmetic operations

--- Java/OperacionDos.java
+++ Java/OperacionDos.java
@@ -6,22 +6,22 @@ public class OperacionDos {
     Scanner sc = new Scanner(System.in);
 
     System.out.println("Ingrese el primer número; ");
-    int num1 = sc.nextInt();
+    float num1 = sc.nextInt();
 
     System.out.println("Ingrese el segundo número; ");
-    int num2 = sc.nextInt();
+    float num2 = sc.nextInt();
 
     if (num1 > num2) {
-      int suma = num1 + num2;
+      float suma = num1 + num2;
       System.out.println("La suma de los números es: " + suma);
 
-      int diferencia = num1 - num2;
+      float diferencia = num1 - num2;
       System.out.println("La diferencia entre los números es: " + diferencia);
     } else {
-      int producto = num1 * num2;
+      float producto = num1 * num2;
       System.out.println("El producto de los números es: " + producto);
 
-      int division = num2 / num1;
+      float division = num1 / num2;
       System.out.println("La división entre los números es: " + division);
     }
 
