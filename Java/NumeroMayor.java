052a6132188d065c7e5486c9fa1141bf3380811a
Espadv69 <aitorjob69@gmail.com>
2025-03-06 13:17:28 +0100

    Enhance NumeroMayor logic to display both greater and lesser numbers

--- Java/NumeroMayor.java
+++ Java/NumeroMayor.java
@@ -11,12 +11,14 @@ public class NumeroMayor {
     System.out.println("Ingrese el segundo número; ");
     int num2 = sc.nextInt();
 
+    int numMayor = Math.max(num1, num2);
+    int numMin = Math.min(num1, num2);
+
     if (num1 == num2) {
       System.out.println("Los números son iguales");
-    } else if (num1 > num2) {
-      System.out.println("El primer número es mayor que el segundo");
     } else {
-      System.out.println("El segundo número es mayor que el primero");
+      System.out.println("El número mayor es: " + numMayor);
+      System.out.println("El número menor es: " + numMin);
     }
 
     sc.close();
