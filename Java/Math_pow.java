e76315b5d396f8d2897afb2e6e53ff669b7d5675
Espadv69 <aitorjob69@gmail.com>
2025-03-06 11:36:59 +0100

    Add Math_pow class to calculate power of a number

---
+++ Java/Math_pow.java
@@ -0,0 +1,14 @@
+public class Math_pow {
+
+	public static void main(String[] args) {
+		
+		double base = 5;
+		double exponente = 3;
+		double resultado = Math.pow(base, exponente);
+		
+		System.out.print("Resultado de " + base + " elevado al exponente " + exponente + " es " + resultado);
+		
+
+	}
+
+}
