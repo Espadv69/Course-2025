7c32ee9f809fbde3c36ef6a1ee1fed0240c6e749
Espadv69 <aitorjob69@gmail.com>
2025-03-06 12:44:34 +0100

    Fix division in OperacionCombinada class to ensure floating-point calculation

--- Java/OperacionCombinada.java
+++ Java/OperacionCombinada.java
@@ -2,7 +2,7 @@ public class OperacionCombinada {
 
   public static void main(String[] args) {
     int x = 2;
-		float total = (7 * (3 * x + 2) - 5 * (4 * x - 3) - 1) / 4;
+		float total = (7 * (3 * x + 2) - 5 * (4 * x - 3) - 1) / 4.0f;
 		
 		System.out.print(total);
   }
