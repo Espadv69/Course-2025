9006d9af8dc5195679b9848c6495db8aec15bf66
Espadv69 <aitorjob69@gmail.com>
2025-03-06 12:28:59 +0100

    Refactor salary calculation logic in Sueldo class for clarity and maintainability

--- Java/Sueldo.java
+++ Java/Sueldo.java
@@ -1,14 +1,17 @@
 public class Sueldo {
-  
+
   public static void main(String[] args) {
     int sueldo = 1000;
-		int gasto1 = 234;
-		float gasto2 = 34.45f;
-		float gasto3 = 12.99f;
-		
-		float sueldoRestante = sueldo - (gasto1 + gasto2 + gasto3);
-		
-		System.out.println("El sueldo restante es: " + sueldoRestante);
+
+    int gasto1 = 234;
+    float gasto2 = 34.45f;
+    float gasto3 = 12.99f;
+
+    float gastoTotal = gasto1 + gasto2 + gasto3;
+
+    float sueldoRestante = sueldo - gastoTotal;
+
+    System.out.println("El sueldo restante es: " + sueldoRestante);
   }
 
 }
