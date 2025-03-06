9eea79a45ec1129aaf952da13ab0ae6ec85099c6
Espadv69 <aitorjob69@gmail.com>
2025-03-06 12:18:18 +0100

    Add calculation logic to OperacionCombinada class

--- Java/OperacionCombinada.java
+++ Java/OperacionCombinada.java
@@ -1,7 +1,10 @@
 public class OperacionCombinada {
 
   public static void main(String[] args) {
-    
+    int x = 2;
+		float total = (7 * (3 * x + 2) - 5 * (4 * x - 3) - 1) / 4;
+		
+		System.out.print(total);
   }
 
 }
