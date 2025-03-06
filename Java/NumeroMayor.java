82069023e32c51c238a482e1cd96861b33b2d4d6
Espadv69 <aitorjob69@gmail.com>
2025-03-06 12:56:52 +0100

    Add number comparison logic in NumeroMayor main method

--- Java/NumeroMayor.java
+++ Java/NumeroMayor.java
@@ -4,6 +4,21 @@ public class NumeroMayor {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+
+    System.out.println("Ingrese el primer número; ");
+    int num1 = sc.nextInt();
+
+    System.out.println("Ingrese el segundo número; ");
+    int num2 = sc.nextInt();
+
+    if (num1 == num2) {
+      System.out.println("Los números son iguales");
+    } else if (num1 > num2) {
+      System.out.println("El primer número es mayor que el segundo");
+    } else {
+      System.out.println("El segundo número es mayor que el primero");
+    }
+
     sc.close();
   }
 
