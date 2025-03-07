517e4d7942c3ee87a48ad15314abd629cd072f8c
Espadv69 <aitorjob69@gmail.com>
2025-03-07 10:51:31 +0100

    Refactor PracticaForm to initialize layout and set size in constructor

--- Java/swing/PracticaForm.java
+++ Java/swing/PracticaForm.java
@@ -2,8 +2,14 @@ package swing;
 
 public class PracticaForm extends javax.swing.JFrame {
 
-  public static void main(String[] args) {
+  public PracticaForm() {
+    setLayout(null);
+  }
 
+  public static void main(String[] args) {
+    PracticaForm form = new PracticaForm();
+    form.setSize(400, 300);
+    form.setVisible(true);
   }
 
 }
