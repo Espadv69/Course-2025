67de66f69d0f2f55ff3a9ad971093c97c6b1c33e
Espadv69 <aitorjob69@gmail.com>
2025-03-07 13:43:16 +0100

    Add JComboBox to FormManejoCampos class for user input selection

--- Java/swing/FormManejoCampos.java
+++ Java/swing/FormManejoCampos.java
@@ -3,9 +3,20 @@ package swing;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 
 public class FormManejoCampos extends JFrame implements ActionListener {
+
+  private JComboBox<String> combo;
+
+  public FormManejoCampos() {
+    setLayout(null);
+
+    combo = new JComboBox<String>();
+    combo.setBounds(10, 10, 80, 20);
+    add(combo);
+  }
 
   public static void main(String[] args) {
 
