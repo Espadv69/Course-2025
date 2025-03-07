9ca3cad86080013344bf827e1e264b31dcb06201
Espadv69 <aitorjob69@gmail.com>
2025-03-07 09:22:37 +0100

    Add OperacionCombinada class to perform a combined mathematical operation and display the result

---
+++ Java/Math/OperacionCombinada.java
@@ -0,0 +1,10 @@
+public class OperacionCombinada {
+
+  public static void main(String[] args) {
+    int x = 2;
+		float total = (7 * (3 * x + 2) - 5 * (4 * x - 3) - 1) / 4.0f;
+		
+		System.out.print(total);
+  }
+
+}
