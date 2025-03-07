b547913f5917c872b5fbe08dbc8c5b61ff8d9959
Espadv69 <aitorjob69@gmail.com>
2025-03-07 11:56:21 +0100

    Refactor PracticaFormEvent to extend JFrame and add buttons for user interaction

--- Java/swing/PracticaFormEvent.java
+++ Java/swing/PracticaFormEvent.java
@@ -1,9 +1,25 @@
 package swing;
 
-public class PracticaFormEvent {
+import javax.swing.JButton;
+
+public class PracticaFormEvent extends javax.swing.JFrame {
+
+  private JButton btnAceptar, btnCancelar;
+
+  public PracticaFormEvent() {
+    setLayout(null);
+
+    btnAceptar = new JButton("Aceptar");
+    btnAceptar.setBounds(10, 10, 100, 30);
+    add(btnAceptar);
+
+    btnCancelar = new JButton("Cancelar");
+    btnCancelar.setBounds(120, 10, 100, 30);
+    add(btnCancelar);
+  }
 
   public static void main(String[] args) {
-
+    
   }
 
 }
