8bd379c5c4df80bf656ea39bf0547a11f03db59a
Espadv69 <aitorjob69@gmail.com>
2025-03-07 11:38:55 +0100

    Refactor PracticaFormConstructor to extend JFrame and initialize labels in constructor

--- Java/swing/PracticaFormConstructor.java
+++ Java/swing/PracticaFormConstructor.java
@@ -1,9 +1,29 @@
 package swing;
 
-public class PracticaFormConstructor {
+import javax.swing.JLabel;
+
+public class PracticaFormConstructor extends javax.swing.JFrame {
+
+  private javax.swing.JLabel lblTitulo, lblVersion;
+
+  public PracticaFormConstructor() {
+    setLayout(null);
+
+    lblTitulo = new JLabel("Sistema de Facturación");
+    lblTitulo.setBounds(10, 10, 200, 30);
+    add(lblTitulo);
+
+    lblVersion = new JLabel("Java Version: 23.0.2");
+    lblVersion.setBounds(10, 40, 200, 30);
+    add(lblVersion);
+  }
 
   public static void main(String[] args) {
-
+    PracticaFormConstructor form = new PracticaFormConstructor();
+    form.setBounds(10, 10, 400, 300);
+    form.setVisible(true);
+    form.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    form.setTitle("Practica Formulario");
   }
 
 }
