82508be85a2208d583f7a0a8ed4ffb4f674d480b
Espadv69 <aitorjob69@gmail.com>
2025-03-07 12:12:30 +0100

    Simplify import statement for JFrame in PracticaFormEvent class

--- Java/swing/PracticaFormEvent.java
+++ Java/swing/PracticaFormEvent.java
@@ -4,8 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JFrame;
 
-public class PracticaFormEvent extends javax.swing.JFrame implements ActionListener {
+public class PracticaFormEvent extends JFrame implements ActionListener {
 
   private JButton btnAceptar, btnCancelar;
 
