b933da710b35d06f22fe417af257e6467ebf7ee9
Espadv69 <aitorjob69@gmail.com>
2025-03-07 09:50:03 +0100

    Add Scanner import and initialization in Trimestre class

--- Java/LogicOperator/Trimestre.java
+++ Java/LogicOperator/Trimestre.java
@@ -1,9 +1,13 @@
 package LogicOperator;
 
+import java.util.Scanner;
+
 public class Trimestre {
 
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
+    sc.close();
   }
 
 }
