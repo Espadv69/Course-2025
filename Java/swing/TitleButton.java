3cd0df9d3fcd90ebb2a0b994096403acb673f19e
Espadv69 <aitorjob69@gmail.com>
2025-03-07 14:18:15 +0100

    Add ActionListener to buttons in TitleButton class for dynamic title updates

--- Java/swing/TitleButton.java
+++ Java/swing/TitleButton.java
@@ -15,14 +15,17 @@ public class TitleButton extends JFrame implements ActionListener {
 
     btn1 = new JButton("Button 1");
     btn1.setBounds(10, 10, 100, 30);
+    btn1.addActionListener(this);
     add(btn1);
 
     btn2 = new JButton("Button 2");
     btn2.setBounds(10, 50, 100, 30);
+    btn2.addActionListener(this);
     add(btn2);
 
     btn3 = new JButton("Button 3");
     btn3.setBounds(10, 90, 100, 30);
+    btn3.addActionListener(this);
     add(btn3);
   }
 
@@ -36,7 +39,13 @@ public class TitleButton extends JFrame implements ActionListener {
 
   @Override
   public void actionPerformed(ActionEvent e) {
-    
+    if (e.getSource() == btn1) {
+      setTitle("Button 1");
+    } else if (e.getSource() == btn2) {
+      setTitle("Button 2");
+    } else if (e.getSource() == btn3) {
+      setTitle("Button 3");
+    }
   }
 
 }
