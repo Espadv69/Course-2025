f11f55d7fabbd371fa7999cf782bab76750097a6
Espadv69 <aitorjob69@gmail.com>
2025-03-07 13:41:41 +0100

    Implement action handling in FormManejoCampos class with error handling

--- Java/swing/FormManejoCampos.java
+++ Java/swing/FormManejoCampos.java
@@ -1,9 +1,23 @@
 package swing;
 
-public class FormManejoCampos {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class FormManejoCampos extends JFrame implements ActionListener {
 
   public static void main(String[] args) {
 
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    try {
+
+    } catch (Exception err) {
+      err.printStackTrace();
+    }
   }
 
 }
