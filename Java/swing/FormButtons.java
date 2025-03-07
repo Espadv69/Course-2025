b4fb879801fac61881d82ae85d0c2bdb25287ae7
Espadv69 <aitorjob69@gmail.com>
2025-03-07 12:25:39 +0100

    Implement FormButtons class with basic GUI setup and ActionListener

--- Java/swing/FormButtons.java
+++ Java/swing/FormButtons.java
@@ -1,9 +1,38 @@
 package swing;
 
-public class FormButtons {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class FormButtons extends JFrame implements ActionListener {
+
+  private JLabel lblName;
+  private JTextField txtName;
+  private JButton btnSubmit, btnReset;
+
+  public FormButtons() {
+    setLayout(null);
+  }
 
   public static void main(String[] args) {
+    FormButtons form = new FormButtons();
+    form.setSize(300, 200);
+    form.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    form.setVisible(true);
+    form.setTitle("Formulario con Botones");
+  }
 
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    try {
+
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
   }
 
 }
