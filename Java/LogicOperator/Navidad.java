6c5961e765a7fb06c800c16b302562227131c52f
Espadv69 <aitorjob69@gmail.com>
2025-03-07 10:09:57 +0100

    Add logic to check for Christmas in Navidad class

--- Java/LogicOperator/Navidad.java
+++ Java/LogicOperator/Navidad.java
@@ -7,6 +7,18 @@ public class Navidad {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
+    System.out.println("Ingrese el número de dia: ");
+    int day = sc.nextInt();
+
+    System.out.println("Ingrese el número de mes: ");
+    int month = sc.nextInt();
+
+    if (day == 25 && month == 12) {
+      System.out.println("¡Feliz Navidad!");
+    } else {
+      System.out.println("Hoy no es Navidad.");
+    }
+
     sc.close();
   }
 
