fb1e8c864febf0997ca72e7623dd48dccb0358c4
Espadv69 <aitorjob69@gmail.com>
2025-03-07 14:11:34 +0100

    Implement TitleButton class as a JPanel and add ActionListener for future event handling

--- Java/swing/TitleButton.java
+++ Java/swing/TitleButton.java
@@ -1,9 +1,23 @@
 package swing;
 
-public class TitleButton {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class TitleButton extends JPanel implements ActionListener {
+
+  public TitleButton() {
+    
+  }
 
   public static void main(String[] args) {
 
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    
   }
 
 }
