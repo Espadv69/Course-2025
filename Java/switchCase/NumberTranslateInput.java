0f5653f94871de409040e92cfaae9c7eaefbb061
Espadv69 <aitorjob69@gmail.com>
2025-03-10 10:14:22 +0100

    Implement GUI for NumberTranslateInput with JFrame setup and action listener

--- Java/switchCase/NumberTranslateInput.java
+++ Java/switchCase/NumberTranslateInput.java
@@ -1,8 +1,33 @@
 package switchCase;
 
-public class NumberTranslateInput {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class NumberTranslateInput extends JFrame implements ActionListener {
+
+  private JLabel lblNumber, lblResult;
+  private JTextField txtNumber;
+  private JButton btnTranslate;
+
+  public NumberTranslateInput() {
+    setLayout(null);
+  }
 
   public static void main(String[] args) {
+    NumberTranslateInput frame = new NumberTranslateInput();
+    frame.setTitle("Number Translate");
+    frame.setSize(300, 200);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
 
   }
 
