7f3d63c4fae57e0fadea74b12fa98b9564be9f39
Espadv69 <aitorjob69@gmail.com>
2025-03-10 12:46:59 +0100

    Refactor input handling and validation in PiecesLength to streamline user prompts and improve length checks

--- Java/loops/PiecesLength.java
+++ Java/loops/PiecesLength.java
@@ -7,37 +7,25 @@ public class PiecesLength {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    System.out.println("Enter the number of pieces to process:");
-
-    while (!sc.hasNextInt()) {
-      System.out.println("The number of pieces must be an integer!");
-      sc.next();
-    }
-
+    System.out.println("Enter the number of pieces to be processed;");
     int pieces = sc.nextInt();
     sc.nextLine();
 
+    float length;
+
     while (pieces > 0) {
       System.out.println("Enter the length of the piece:");
+      length = sc.nextFloat();
 
-      while (!sc.hasNextDouble()) {
-        System.out.println("The length of the piece must be a number!");
-        sc.next();
-      }
-
-      double length = sc.nextDouble();
-      sc.nextLine();
-
-      if (length < 1.0) {
-        System.out.println("The length of the piece is too small!");
-      } else if (length > 2.0) {
-        System.out.println("The length of the piece is too large!");
+      if (length >= 1.0 && length <= 2.0) {
+        System.out.println("The piece is in the correct length.");
       } else {
-        System.out.println("The piece is accepted!");
+        System.out.println("The piece is not in the correct length.");
       }
 
       pieces--;
     }
+
     System.out.println("Processing is complete!");
 
     sc.close();
