4d313a4de876cf76bc17e2dd497e7a008c72d2f1
Espadv69 <aitorjob69@gmail.com>
2025-03-10 12:19:42 +0100

    Add piece length validation logic in PiecesLength main method

--- Java/loops/PiecesLength.java
+++ Java/loops/PiecesLength.java
@@ -7,6 +7,26 @@ public class PiecesLength {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
+    System.out.println("Enter the number of pieces to process:");
+    int pieces = sc.nextInt();
+
+    float length;
+
+    while (pieces > 0) {
+      System.out.println("Enter the length of the piece:");
+      length = sc.nextFloat();
+
+      if (length < 1.0) {
+        System.out.println("The piece is too small!");
+      } else if (length > 2.0) {
+        System.out.println("The piece is too large!");
+      } else {
+        System.out.println("The piece is accepted!");
+      }
+
+      pieces--;
+    }
+
     sc.close();
   }
 
