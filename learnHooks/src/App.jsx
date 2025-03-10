8b8de72d8c88670f46ea1daa0c039fc4e7b32677
Espadv69 <aitorjob69@gmail.com>
2025-03-10 09:50:08 +0100

    Refactor App component to use Counter component for better code organization

--- learnHooks/src/App.jsx
+++ learnHooks/src/App.jsx
@@ -1,31 +1,10 @@
-import { useState } from 'react'
+import Counter from './components/Counter'
 
 const App = () => {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="bg-blue-600 p-4 w-full h-screen flex flex-col items-center justify-center gap-4 text-white">
-      <h1 className="text-5xl">Counter</h1>
-      <p className="text-3xl">{count}</p>
-      <button
-        className="border-2 p-4 w-[200px] cursor-pointer"
-        onClick={() => setCount(count + 1)}
-      >
-        Increment
-      </button>
-      <button
-        className="border-2 p-4 w-[200px] cursor-pointer"
-        onClick={() => setCount(count - 1)}
-      >
-        Decrement
-      </button>
-      <button
-        className="border-2 p-4 w-[200px] cursor-pointer"
-        onClick={() => setCount(0)}
-      >
-        Reset
-      </button>
-    </div>
+    <section>
+      <Counter />
+    </section>
   )
 }
 
