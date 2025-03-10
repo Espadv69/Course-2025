91299fbdcf6e61d733bcd7ae44826f74e3a3a5ee
Espadv69 <aitorjob69@gmail.com>
2025-03-10 11:40:42 +0100

    Implement number guessing game logic in GuessRandomLoop class

--- Java/loops/GuessRandomLoop.java
+++ Java/loops/GuessRandomLoop.java
@@ -7,6 +7,26 @@ public class GuessRandomLoop {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
+    // Guess a number between 1 to 100
+    int number = (int) (Math.random() * 100) + 1;
+    int guess = 0;
+
+    System.out.println("Guess a number between 1 to 100");
+
+    while (guess != number) {
+      guess = sc.nextInt();
+
+      if (guess == number) {
+        System.out.println("Congratulations! You guessed the number");
+      } else if (guess < number) {
+        System.out.println("Try a higher number");
+      } else {
+        System.out.println("Try a lower number");
+      }
+
+      sc.close();
+    }
+
     sc.close();
   }
 
