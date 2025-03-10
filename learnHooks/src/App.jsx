87d91abd81d35ee0bab41af93d2a1d05da8c1c95
Espadv69 <aitorjob69@gmail.com>
2025-03-10 09:46:52 +0100

    Implement counter functionality in App component with increment, decrement, and reset buttons

--- learnHooks/src/App.jsx
+++ learnHooks/src/App.jsx
@@ -1,3 +1,32 @@
-const App = () => {}
+import { useState } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-blue-300 p-4 w-full h-screen flex flex-col items-center justify-center gap-4 text-white">
+      <h1 className="text-5xl">Counter</h1>
+      <p className="text-3xl">{count}</p>
+      <button
+        className="border-2 p-4 w-[200px] cursor-pointer"
+        onClick={() => setCount(count + 1)}
+      >
+        Increment
+      </button>
+      <button
+        className="border-2 p-4 w-[200px] cursor-pointer"
+        onClick={() => setCount(count - 1)}
+      >
+        Decrement
+      </button>
+      <button
+        className="border-2 p-4 w-[200px] cursor-pointer"
+        onClick={() => setCount(0)}
+      >
+        Reset
+      </button>
+    </div>
+  )
+}
 
 export default App
