fd8575a54547e750584e768c84bb68db7798e9c4
Espadv69 <aitorjob69@gmail.com>
2025-03-10 13:00:08 +0100

    Add Form component with name and age input handling

--- learnHooks/src/components/Form.jsx
+++ learnHooks/src/components/Form.jsx
@@ -1,3 +1,34 @@
-const Form = () => {}
+import { useState } from 'react'
+
+const Form = () => {
+  const [name, setName] = useState('')
+  const [age, setAge] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    alert(`Name: ${name} and Age: ${age}`)
+    setName('')
+    setAge('')
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Age"
+        value={age}
+        onChange={(e) => setAge(e.target.value)}
+      />
+      <button>Submit</button>
+    </form>
+  )
+}
 
 export default Form
