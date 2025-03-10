cca384a83b40e7cbc8812a7acf06411a665d6e38
Espadv69 <aitorjob69@gmail.com>
2025-03-10 10:45:44 +0100

    Add UI components to PasswordLoop class for user input

--- Java/loops/PasswordLoop.java
+++ Java/loops/PasswordLoop.java
@@ -3,12 +3,19 @@ package loops;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class PasswordLoop extends JFrame implements ActionListener {
 
-  public PasswordLoop() {
+  private JLabel userLabel, passLabel;
+  private JTextField userField, passField;
+  private JButton loginButton;
 
+  public PasswordLoop() {
+    setLayout(null);
   }
 
   public static void main(String[] args) {
