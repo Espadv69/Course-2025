cf9c62cc934fcfcd5e9886489c5583ba349cdadb
Espadv69 <aitorjob69@gmail.com>
2025-03-10 09:59:11 +0100

    Add number translation functionality to NumberTranslate class

--- Java/switchCase/NumberTranslate.java
+++ Java/switchCase/NumberTranslate.java
@@ -7,6 +7,41 @@ public class NumberTranslate {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
+    System.out.print("Enter a number: ");
+    int number = sc.nextInt();
+
+    switch (number) {
+      case 1:
+        System.out.println("One");
+        break;
+      case 2:
+        System.out.println("Two");
+        break;
+      case 3:
+        System.out.println("Three");
+        break;
+      case 4:
+        System.out.println("Four");
+        break;
+      case 5:
+        System.out.println("Five");
+        break;
+      case 6:
+        System.out.println("Six");
+        break;
+      case 7:
+        System.out.println("Seven");
+        break;
+      case 8:
+        System.out.println("Eight");
+        break;
+      case 9:
+        System.out.println("Nine");
+        break;
+      default:
+        System.out.println("Invalid number, please enter a number between 1 to 9.");
+    }
+
     sc.close();
   }
 
