68220a301164dac1b529dacfc476286b316e7a6a
Espadv69 <aitorjob69@gmail.com>
2025-03-10 13:04:45 +0100

    Enhance Form component styling with background, padding, and border improvements

--- learnHooks/src/components/Form.jsx
+++ learnHooks/src/components/Form.jsx
@@ -13,20 +13,25 @@ const Form = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      onSubmit={handleSubmit}
+      className="bg-yellow-300 text-black p-4 w-full h-screen flex flex-col justify-center items-center gap-4"
+    >
       <input
         type="text"
+        className="p-2 border-2 border-black"
         placeholder="Name"
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
       <input
         type="number"
+        className="p-2 border-2 border-black"
         placeholder="Age"
         value={age}
         onChange={(e) => setAge(e.target.value)}
       />
-      <button>Submit</button>
+      <button className="p-4 border-2 rounded">Submit</button>
     </form>
   )
 }
