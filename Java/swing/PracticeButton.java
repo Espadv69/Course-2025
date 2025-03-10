b932a0740ed1663611270e969af59d295003b418
Espadv69 <aitorjob69@gmail.com>
2025-03-10 13:43:21 +0100

    Refactor PracticeButton to extend JFrame and add a JButton with layout configuration

--- Java/swing/PracticeButton.java
+++ Java/swing/PracticeButton.java
@@ -1,8 +1,27 @@
 package swing;
 
-public class PracticeButton {
-  public static void main(String[] args) {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
+public class PracticeButton extends JFrame {
+
+  private JButton button;
+  private JLabel text;
+
+  public PracticeButton() {
+    setLayout(null);
+  
+    button = new JButton("Click me!");
+    button.setBounds(100, 100, 100, 50);
+    add(button);
+  }
+
+  public static void main(String[] args) {
+    PracticeButton frame = new PracticeButton();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(300, 300);
+    frame.setVisible(true);
   }
 
 }
