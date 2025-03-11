9b5483a7fef713189ca7c9d8a20905852a6c3f1e
Espadv69 <aitorjob69@gmail.com>
2025-03-11 12:01:42 +0100

    Refactor MenuDrop to extend JFrame and initialize window properties

--- Java/swing/MenuDrop.java
+++ Java/swing/MenuDrop.java
@@ -1,9 +1,19 @@
 package swing;
 
-public class MenuDrop {
+import javax.swing.JFrame;
+
+public class MenuDrop extends JFrame {
+
+  public MenuDrop() {
+    JFrame form = new JFrame("Menu Drop");
+    form.setSize(300, 200);
+    form.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    form.setVisible(true);
+  }
 
   public static void main(String[] args) {
-
+    new MenuDrop();
   }
 
 }
