bca786586b8c9b8e5c54ba18ea71a9881a0e72ca
Espadv69 <aitorjob69@gmail.com>
2025-03-11 12:13:52 +0100

    Implement action listeners for menu items in MenuDrop for enhanced interactivity

--- Java/swing/MenuDrop.java
+++ Java/swing/MenuDrop.java
@@ -1,11 +1,14 @@
 package swing;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
-public class MenuDrop extends JFrame {
+public class MenuDrop extends JFrame implements ActionListener {
 
   private JMenuBar menuBar;
   private JMenu menuFile, menuEdit, menuHelp;
@@ -31,15 +34,28 @@ public class MenuDrop extends JFrame {
     menuBar.add(menuHelp);
 
     itemNew = new JMenuItem("New");
+    itemNew.addActionListener(this);
+
     itemOpen = new JMenuItem("Open");
+    itemOpen.addActionListener(this);
+
     itemSave = new JMenuItem("Save");
+    itemSave.addActionListener(this);
+
     itemExit = new JMenuItem("Exit");
+    itemExit.addActionListener(this);
 
     itemCut = new JMenuItem("Cut");
+    itemCut.addActionListener(this);
+
     itemCopy = new JMenuItem("Copy");
+    itemCopy.addActionListener(this);
+
     itemPaste = new JMenuItem("Paste");
+    itemPaste.addActionListener(this);
 
     itemAbout = new JMenuItem("About");
+    itemAbout.addActionListener(this);
 
     menuFile.add(itemNew);
     menuFile.add(itemOpen);
@@ -57,6 +73,27 @@ public class MenuDrop extends JFrame {
 
   public static void main(String[] args) {
     new MenuDrop();
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    if (e.getSource() == itemNew) {
+      System.out.println("New");
+    } else if (e.getSource() == itemOpen) {
+      System.out.println("Open");
+    } else if (e.getSource() == itemSave) {
+      System.out.println("Save");
+    } else if (e.getSource() == itemExit) {
+      System.out.println("Exit");
+    } else if (e.getSource() == itemCut) {
+      System.out.println("Cut");
+    } else if (e.getSource() == itemCopy) {
+      System.out.println("Copy");
+    } else if (e.getSource() == itemPaste) {
+      System.out.println("Paste");
+    } else if (e.getSource() == itemAbout) {
+      System.out.println("About");
+    }
   }
 
 }
