93b29e1062ce543bfce1a4e82fa97505bb86bceb
Espadv69 <aitorjob69@gmail.com>
2025-03-11 10:12:46 +0100

    Refactor JTextOperation to extend JFrame and initialize the frame in the constructor

--- Java/JTextField/JTextOperation.java
+++ Java/JTextField/JTextOperation.java
@@ -1,9 +1,20 @@
 package JTextField;
 
-public class JTextOperation {
+import javax.swing.JFrame;
+
+public class JTextOperation extends JFrame {
+
+  public JTextOperation() {
+    JFrame frame = new JFrame("JTextOperation");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(400, 400);
+    frame.setLayout(null);
+
+    frame.setVisible(true);
+  }
 
   public static void main(String[] args) {
-
+    new JTextOperation();
   }
 
 }
