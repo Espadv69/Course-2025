e6cc78bbbaedc9f65185e00fb319b666abbfcfd6
Espadv69 <aitorjob69@gmail.com>
2025-03-11 13:01:01 +0100

    Refactor TablaTutorial to extend JFrame and initialize the GUI in the constructor

--- Java/swing/TablaTutorial.java
+++ Java/swing/TablaTutorial.java
@@ -1,9 +1,19 @@
 package swing;
 
-public class TablaTutorial {
+import javax.swing.JFrame;
+
+public class TablaTutorial extends JFrame {
+
+  public TablaTutorial() {
+    JFrame form = new JFrame("Tabla Tutorial");
+    form.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    form.setSize(400, 300);
+
+    form.setVisible(true);
+  }
 
   public static void main(String[] args) {
-
+    new TablaTutorial();
   }
 
 }
