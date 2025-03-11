acdbf8f4c044dca6a01805303b642cba5d5caf24
Espadv69 <aitorjob69@gmail.com>
2025-03-11 13:49:21 +0100

    Implement ProductTable as a JFrame and add ActionListener functionality

--- Java/swing/ProductTable.java
+++ Java/swing/ProductTable.java
@@ -1,8 +1,22 @@
 package swing;
 
-public class ProductTable {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class ProductTable extends JFrame implements ActionListener {
+
+  public ProductTable() {
+
+  }
 
   public static void main(String[] args) {
+
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
 
   }
 
