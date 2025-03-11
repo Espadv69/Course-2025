e8bffaa3c9e0e539282dc04c687a4f3f4dc2d6d8
Espadv69 <aitorjob69@gmail.com>
2025-03-11 13:07:55 +0100

    Add JTable with sample data to TablaTutorial for enhanced GUI functionality

--- Java/swing/TablaTutorial.java
+++ Java/swing/TablaTutorial.java
@@ -1,13 +1,32 @@
 package swing;
 
 import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
 
 public class TablaTutorial extends JFrame {
+
+  String[] columnas = { "Nombre", "Apellido", "Edad" };
+  Object[][] datos = {
+      { "Juan", "Perez", 25 },
+      { "Maria", "Gomez", 30 },
+      { "Pedro", "Garcia", 35 },
+      { "Jose", "Luis", 52 }
+  };
 
   public TablaTutorial() {
     JFrame form = new JFrame("Tabla Tutorial");
     form.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     form.setSize(400, 300);
+
+    // Crear la tabla con los datos y columnas
+    JTable tabla = new JTable(datos, columnas);
+
+    // Agregar la tabla a un JScrollPane para que tenga barra de desplazamiento
+    JScrollPane scroll = new JScrollPane(tabla);
+
+    // Agregar el JScrollPane al formulario
+    form.add(scroll);
 
     form.setVisible(true);
   }
