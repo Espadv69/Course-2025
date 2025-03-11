a54a60f9a147f20ab03166a7f046e0b3b00dc5ef
Espadv69 <aitorjob69@gmail.com>
2025-03-11 12:04:42 +0100

    Add menu components to MenuDrop class for enhanced functionality

--- Java/swing/MenuDrop.java
+++ Java/swing/MenuDrop.java
@@ -1,8 +1,18 @@
 package swing;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class MenuDrop extends JFrame {
+
+  private JMenuBar menuBar;
+  private JMenu menuFile, menuEdit, menuHelp;
+
+  private JMenuItem itemNew, itemOpen, itemSave, itemExit;
+  private JMenuItem itemCut, itemCopy, itemPaste;
+  private JMenuItem itemAbout;
 
   public MenuDrop() {
     JFrame form = new JFrame("Menu Drop");
