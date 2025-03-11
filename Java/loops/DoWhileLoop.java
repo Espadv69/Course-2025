12b0b6d43ddb3419c10947a71ad3c7369e5f3391
Espadv69 <aitorjob69@gmail.com>
2025-03-11 14:13:45 +0100

    Modify DoWhileLoop to accept user input for dynamic number printing

--- Java/loops/DoWhileLoop.java
+++ Java/loops/DoWhileLoop.java
@@ -1,15 +1,22 @@
 package loops;
 
+import java.util.Scanner;
+
 public class DoWhileLoop {
 
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-    int i = 1;
+    System.out.println("Enter a number: ");
+    int i = sc.nextInt();
+    int counter = 1;
+
     do {
-      System.out.println(i);
-      i++;
-    } while (i <= 10);
+      System.out.println(counter);
+      counter++;
+    } while (counter <= i);
 
+    sc.close();
   }
 
 }
