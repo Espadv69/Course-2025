70668db555ec201ff7aae204055cc6a6771cb535
Espadv69 <aitorjob69@gmail.com>
2025-03-12 10:08:26 +0100

    Add JTable with default data to Tabla class

--- Java/Apps/Tabla.java
+++ Java/Apps/Tabla.java
@@ -1,15 +1,29 @@
 package Apps;
 
 import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class Tabla extends JFrame {
 
   public JFrame frame;
+  public DefaultTableModel model;
 
   public Tabla() {
     frame = new JFrame("Tabla");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(400, 300);
+
+    model = new DefaultTableModel();
+    model.addColumn("Nombre");
+    model.addColumn("Edad");
+    model.addColumn("Sexo");
+    model.addColumn("Pais");
+
+    model.addRow(new Object[] { "Juan", 25, "M", "Mexico" });
+    model.addRow(new Object[] { "Maria", 22, "F", "Mexico" });
+
+    frame.add(new JTable(model));
 
     frame.setVisible(true);
   }
