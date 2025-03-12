747bd06685ce3f01b46e1f8f0bf4e64c2d770e3a
Espadv69 <aitorjob69@gmail.com>
2025-03-12 10:19:50 +0100

    Add input fields for name, age, and gender in Tabla class

--- Java/Apps/Tabla.java
+++ Java/Apps/Tabla.java
@@ -1,15 +1,20 @@
 package Apps;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
 public class Tabla extends JFrame {
 
-  public JFrame frame;
-  public DefaultTableModel model;
-  public JScrollPane sp;
+  private JPanel panel;
+  private JTextField txtNombre, txtEdad, txtSexo;
+
+  private JFrame frame;
+  private DefaultTableModel model;
+  private JScrollPane sp;
 
   public Tabla() {
     frame = new JFrame("Tabla");
@@ -26,6 +31,21 @@ public class Tabla extends JFrame {
 
     model.addRow(new Object[] { "Juan", 25, "M" });
     model.addRow(new Object[] { "Maria", 22, "F" });
+
+    panel = new JPanel();
+    panel.setLayout(null);
+
+    txtNombre = new JTextField();
+    txtNombre.setBounds(10, 10, 100, 20);
+    panel.add(txtNombre);
+
+    txtEdad = new JTextField();
+    txtEdad.setBounds(120, 10, 100, 20);
+    panel.add(txtEdad);
+
+    txtSexo = new JTextField();
+    txtSexo.setBounds(230, 10, 100, 20);
+    panel.add(txtSexo);
 
     JTable table = new JTable(model);
     sp.setViewportView(table);
