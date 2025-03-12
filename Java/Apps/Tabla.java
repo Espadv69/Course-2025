ca1046a7d376555bd60a8a1e2400fdc9dcc55d97
Espadv69 <aitorjob69@gmail.com>
2025-03-12 10:11:23 +0100

    Add JScrollPane to Tabla class for better table display

--- Java/Apps/Tabla.java
+++ Java/Apps/Tabla.java
@@ -1,6 +1,7 @@
 package Apps;
 
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
@@ -8,22 +9,27 @@ public class Tabla extends JFrame {
 
   public JFrame frame;
   public DefaultTableModel model;
+  public JScrollPane sp;
 
   public Tabla() {
     frame = new JFrame("Tabla");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(400, 300);
 
+    sp = new JScrollPane();
+    sp.setBounds(10, 10, 375, 250);
+
     model = new DefaultTableModel();
     model.addColumn("Nombre");
     model.addColumn("Edad");
     model.addColumn("Sexo");
-    model.addColumn("Pais");
 
-    model.addRow(new Object[] { "Juan", 25, "M", "Mexico" });
-    model.addRow(new Object[] { "Maria", 22, "F", "Mexico" });
+    JTable table = new JTable(model);
+    sp.setViewportView(table);
+    frame.add(sp);
 
-    frame.add(new JTable(model));
+    model.addRow(new Object[] { "Juan", 25, "M" });
+    model.addRow(new Object[] { "Maria", 22, "F" });
 
     frame.setVisible(true);
   }
