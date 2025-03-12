d34c32d7bfacab1c3415bf37f06b08fdebf2fbcf
Espadv69 <aitorjob69@gmail.com>
2025-03-12 09:59:20 +0100

    Refactor Tabla class to extend JFrame and initialize a new JFrame instance

--- Java/Apps/Tabla.java
+++ Java/Apps/Tabla.java
@@ -1,9 +1,17 @@
 package Apps;
 
-public class Tabla {
+import javax.swing.JFrame;
+
+public class Tabla extends JFrame {
+
+  public JFrame frame;
+
+  public Tabla() {
+    frame = new JFrame("Tabla");
+  }
 
   public static void main(String[] args) {
-
+    new Tabla();
   }
 
 }
