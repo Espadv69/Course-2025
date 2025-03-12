b058b5bb27db1c7b26ca1a6f0f80972ebf366845
Espadv69 <aitorjob69@gmail.com>
2025-03-12 10:00:18 +0100

    Set default close operation and size for Tabla JFrame

--- Java/Apps/Tabla.java
+++ Java/Apps/Tabla.java
@@ -8,6 +8,10 @@ public class Tabla extends JFrame {
 
   public Tabla() {
     frame = new JFrame("Tabla");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(400, 300);
+
+    frame.setVisible(true);
   }
 
   public static void main(String[] args) {
