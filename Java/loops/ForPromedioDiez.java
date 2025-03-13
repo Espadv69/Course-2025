3a9df7ff5a2e38222ee182d197c07970a2938be9
Espadv69 <aitorjob69@gmail.com>
2025-03-13 13:44:16 +0100

    Add Scanner import and close method in ForPromedioDiez class

--- Java/loops/ForPromedioDiez.java
+++ Java/loops/ForPromedioDiez.java
@@ -1,9 +1,12 @@
 package loops;
 
+import java.util.Scanner;
+
 public class ForPromedioDiez {
 
   public static void main(String[] args) {
-
+    Scanner sc = new Scanner(System.in);
+    sc.close();
   }
 
 }
