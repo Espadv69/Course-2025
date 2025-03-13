5e59d36499a6904c46428200b606f422969fbd0f
Espadv69 <aitorjob69@gmail.com>
2025-03-13 13:41:56 +0100

    Enhance ForOneToHundred class to print numbers with a prefix

--- Java/loops/ForOneToHundred.java
+++ Java/loops/ForOneToHundred.java
@@ -3,7 +3,9 @@ package loops;
 public class ForOneToHundred {
 
   public static void main(String[] args) {
-
+    for (int i = 1; i <= 100; i++) {
+      System.out.println("Number: " + i);
+    }
   }
 
 }
