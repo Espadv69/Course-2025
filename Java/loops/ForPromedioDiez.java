ff83159ca1b593377eada23f2a8def413d36d5ee
Espadv69 <aitorjob69@gmail.com>
2025-03-13 13:46:41 +0100

    Calculate total and average of ten user-input numbers in ForPromedioDiez class

--- Java/loops/ForPromedioDiez.java
+++ Java/loops/ForPromedioDiez.java
@@ -6,6 +6,20 @@ public class ForPromedioDiez {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+
+    int total = 0;
+    int promedio = 0;
+
+    for (int i = 1; i <= 10; i++) {
+      System.out.println("Enter a number: ");
+      int number = sc.nextInt();
+      total += number;
+    }
+
+    promedio = total / 10;
+    System.out.println("The total is: " + total);
+    System.out.println("The average is: " + promedio);
+
     sc.close();
   }
 
