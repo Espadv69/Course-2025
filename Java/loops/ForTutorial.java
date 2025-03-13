13371aa839d3f481bb540644211a4acadd686f52
Espadv69 <aitorjob69@gmail.com>
2025-03-13 11:53:39 +0100

    Add user input prompt and call forLoop method in ForTutorial class

--- Java/loops/ForTutorial.java
+++ Java/loops/ForTutorial.java
@@ -7,6 +7,12 @@ public class ForTutorial {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
+    System.out.println("Enter a number: ");
+    int numbers = sc.nextInt();
+    System.out.println("---------------------------------");
+
+    forLoop(numbers);
+
     sc.close();
   }
 
