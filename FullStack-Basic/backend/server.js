aeee29f18920fa84b3c94509b50c7c4b6d7eb997
Espadv69 <aitorjob69@gmail.com>
2025-03-13 10:33:25 +0100

    Add root route and server startup message to server.js

--- FullStack-Basic/backend/server.js
+++ FullStack-Basic/backend/server.js
@@ -6,3 +6,11 @@ app.use(cors())
 app.use(express.json())
 
 const PORT = 5000
+
+app.get('/', async (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`)
+})
