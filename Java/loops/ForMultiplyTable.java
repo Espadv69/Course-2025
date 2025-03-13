61017f341f73792fb27142b5864c0e38c8742d23
Espadv69 <aitorjob69@gmail.com>
2025-03-13 12:03:30 +0100

    Add Scanner import and initialize in ForMultiplyTable class

--- Java/loops/ForMultiplyTable.java
+++ Java/loops/ForMultiplyTable.java
@@ -1,9 +1,13 @@
 package loops;
 
+import java.util.Scanner;
+
 public class ForMultiplyTable {
 
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
+    sc.close();
   }
 
 }
