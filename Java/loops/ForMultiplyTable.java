12d3935c0eaa7ecf3016084bdea75f655432bb06
Espadv69 <aitorjob69@gmail.com>
2025-03-13 12:07:26 +0100

    Add user input for multiplication table in ForMultiplyTable class

--- Java/loops/ForMultiplyTable.java
+++ Java/loops/ForMultiplyTable.java
@@ -7,7 +7,17 @@ public class ForMultiplyTable {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
+    System.out.println("Enter the number to print the table: ");
+    int number = sc.nextInt();
+
+    table(number);
     sc.close();
+  }
+
+  public static void table(int number) {
+    for (int i = 1; i <= 10; i++) {
+      System.out.println(number + " * " + i + " = " + number * i);
+    }
   }
 
 }
