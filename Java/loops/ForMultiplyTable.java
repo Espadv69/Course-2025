e664ce93622c034cc8f99f90c6a3a8b4eb427d8d
Espadv69 <aitorjob69@gmail.com>
2025-03-13 12:35:43 +0100

    Validate user input for multiplication table range in ForMultiplyTable class

--- Java/loops/ForMultiplyTable.java
+++ Java/loops/ForMultiplyTable.java
@@ -7,8 +7,14 @@ public class ForMultiplyTable {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    System.out.println("Enter the number to print the table: ");
+    System.out.println("Enter the number to print the table between 1 to 100");
     int number = sc.nextInt();
+
+    if (number < 0 || number > 100) {
+      System.out.println("Please enter a number between 1 to 100");
+      System.exit(0);
+    }
+
     System.out.println("---------------------------");
 
     table(number);
