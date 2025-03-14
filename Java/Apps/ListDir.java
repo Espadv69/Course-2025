b467d7b2982518f0a84b740923f4efa5d3c58efb
Espadv69 <aitorjob69@gmail.com>
2025-03-14 10:22:26 +0100

    Implement directory listing functionality in ListDir class

--- Java/Apps/ListDir.java
+++ Java/Apps/ListDir.java
@@ -1,9 +1,21 @@
 package Apps;
 
+import java.io.File;
+
 public class ListDir {
 
   public static void main(String[] args) {
+    File dir = new File("C:\\Users\\Mañana\\Documents\\Course-2025");
+    String[] files = dir.list();
 
+    if (files == null) {
+      System.out.println("No files in the directory");
+      return;
+    }
+
+    for (String file : files) {
+      System.out.println(file);
+    }
   }
 
 }
