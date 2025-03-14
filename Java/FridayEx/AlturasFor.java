22af6d651754eaf4a11187f0f52f70d4643ec2b5
Espadv69 <aitorjob69@gmail.com>
2025-03-14 13:31:42 +0100

    Implement height input and average calculation in AlturasFor class

--- Java/FridayEx/AlturasFor.java
+++ Java/FridayEx/AlturasFor.java
@@ -7,6 +7,23 @@ public class AlturasFor {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
+    System.out.println("Introduce el número de alturas a leer: ");
+    int numAlturas = sc.nextInt();
+
+    double promedio = 0;
+    System.out.println("-----------ALTURAS-----------");
+
+    for (int i = 1; i <= numAlturas; i++) {
+      System.out.println("Introduce la altura " + i + ": ");
+      double altura = sc.nextDouble();
+      promedio += altura;
+    }
+
+    promedio /= numAlturas;
+    System.out.println("El promedio de las alturas es: " + promedio);
+
+    System.out.println("-----------------------------");
+
     sc.close();
   }
 
