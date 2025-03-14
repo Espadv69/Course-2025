9d84d22393ffe235aae1255e7a948af88807a4f3
Espadv69 <aitorjob69@gmail.com>
2025-03-14 11:15:08 +0100

    Integrate FetchAudi component into Home page

--- FullStack-Basic/frontend/src/pages/Home.jsx
+++ FullStack-Basic/frontend/src/pages/Home.jsx
@@ -1,3 +1,12 @@
-const Home = () => {}
+import FetchAudi from '../components/FetchAudi/FetchAudi'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <FetchAudi />
+    </div>
+  )
+}
 
 export default Home
