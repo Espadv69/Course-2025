ae6244b67d7a2b8410b9ccb2c2d5784ab599c858
Espadv69 <aitorjob69@gmail.com>
2025-03-14 13:49:58 +0100

    Enhance LoginSwing class by adding UI components and setting up the JFrame

--- Java/FridayEx/LoginSwing.java
+++ Java/FridayEx/LoginSwing.java
@@ -1,11 +1,22 @@
 package FridayEx;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class LoginSwing extends JFrame {
 
-  public LoginSwing() {
+  private JLabel userLabel, passLabel;
+  private JTextField userField, passField;
+  private JButton loginButton;
 
+  public LoginSwing() {
+    JFrame frame = new JFrame("Login Form");
+    frame.setSize(300, 200);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    frame.setVisible(true);
   }
 
   public static void main(String[] args) {
