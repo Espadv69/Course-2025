a0f34effd08d26e27f3a24653d4c00929b5d4183
Espadv69 <aitorjob69@gmail.com>
2025-03-14 14:04:24 +0100

    Add Scanner import and instantiate in GetValue main method

--- Java/FridayEx/GetValue.java
+++ Java/FridayEx/GetValue.java
@@ -1,9 +1,13 @@
 package FridayEx;
 
+import java.util.Scanner;
+
 public class GetValue {
 
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
+    sc.close();
   }
 
 }
