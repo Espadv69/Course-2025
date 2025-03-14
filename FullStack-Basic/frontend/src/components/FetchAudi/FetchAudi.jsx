021d2a4a3d3925079d51a4982fd87b0991f72de4
Espadv69 <aitorjob69@gmail.com>
2025-03-14 11:11:14 +0100

    Implement audio fetching functionality in FetchAudi component

--- FullStack-Basic/frontend/src/components/FetchAudi/FetchAudi.jsx
+++ FullStack-Basic/frontend/src/components/FetchAudi/FetchAudi.jsx
@@ -1,8 +1,24 @@
 import { useState, useEffect } from 'react'
 import './FetchAudi.css'
 
+const LOCAL_URL = 'http://localhost:5000/api/audis'
+
 const FetchAudi = () => {
   const [audis, setAudis] = useState([])
+
+  useEffect(() => {
+    const fetchAudis = async () => {
+      try {
+        const reponse = await fetch(LOCAL_URL)
+        const data = await reponse.json()
+        setAudis(data)
+      } catch (err) {
+        console.error(err)
+      }
+    }
+
+    fetchAudis()
+  }, [])
 }
 
 export default FetchAudi
