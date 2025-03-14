2ceaffb60cd1a08c42205ba2802e9434a954c34f
Espadv69 <aitorjob69@gmail.com>
2025-03-14 13:13:35 +0100

    Add Scanner import and initialization in NotasPromedio class

--- Java/FridayEx/NotasPromedio.java
+++ Java/FridayEx/NotasPromedio.java
@@ -1,9 +1,13 @@
 package FridayEx;
 
+import java.util.Scanner;
+
 public class NotasPromedio {
 
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
+    sc.close();
   }
 
 }
