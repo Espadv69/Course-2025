17ce633f2c03fb65d5d02b9a22f7d0b9cbaf6699
Espadv69 <aitorjob69@gmail.com>
2025-03-14 13:46:22 +0100

    Implement file deletion logic in DeleteFile class

--- Java/FridayEx/DeleteFile.java
+++ Java/FridayEx/DeleteFile.java
@@ -1,9 +1,19 @@
 package FridayEx;
 
+import java.io.File;
+
 public class DeleteFile {
 
   public static void main(String[] args) {
+    String path = "C:\\Users\\Public\\test.txt";
+    File file = new File(path);
 
+    if (file.exists()) {
+      file.delete();
+      System.out.println("El archivo ha sido eliminado.");
+    } else {
+      System.out.println("El archivo no existe.");
+    }
   }
 
 }
