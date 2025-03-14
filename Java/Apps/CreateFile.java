83f507767084d8ebdb882747a6f23c79841b4ac4
Espadv69 <aitorjob69@gmail.com>
2025-03-14 12:11:44 +0100

    Add file existence check and creation logic in CreateFile class

--- Java/Apps/CreateFile.java
+++ Java/Apps/CreateFile.java
@@ -1,11 +1,27 @@
 package Apps;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class CreateFile {
 
   public static void main(String[] args) {
     File file = new File("C:\\Users\\Public\\test.txt");
+
+    if (file.exists()) {
+      System.out.println("File already exists.");
+      System.exit(0);
+    }
+
+    System.out.println("If you see this message, the file does not exist and will be created.");
+
+    try (FileWriter writer = new FileWriter(file)) {
+      System.out.println("File created successfully.");
+      writer.write("Hello World!");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
 }
