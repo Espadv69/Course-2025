d7490b3c4e24308f50b446ee57f1f6478cf0b3d0
Espadv69 <aitorjob69@gmail.com>
2025-03-14 13:17:48 +0100

    Add average calculation functionality in NotasPromedio class

--- Java/FridayEx/NotasPromedio.java
+++ Java/FridayEx/NotasPromedio.java
@@ -7,6 +7,26 @@ public class NotasPromedio {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
+    System.out.println("--------------NOTAS--------------");
+
+    double resultado = 0.0;
+
+    for (int i = 1; i <= 3; i++) {
+      System.out.print("Ingrese la nota " + i + ": ");
+      double nota = sc.nextDouble();
+      if (nota < 0 || nota > 10) {
+        System.out.println("Nota inválida");
+        i--;
+      }
+
+      resultado += nota;
+    }
+
+    resultado /= 3;
+    System.out.println("El promedio de las notas es: " + resultado);
+
+    System.out.println("---------------------------------");
+
     sc.close();
   }
 
