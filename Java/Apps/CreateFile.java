c5de954b07f5e39f546ea141cea9456492b54ccc
Espadv69 <aitorjob69@gmail.com>
2025-03-14 12:01:04 +0100

    Add file creation logic in CreateFile class

--- Java/Apps/CreateFile.java
+++ Java/Apps/CreateFile.java
@@ -1,9 +1,11 @@
 package Apps;
 
+import java.io.File;
+
 public class CreateFile {
 
   public static void main(String[] args) {
-
+    File file = new File("C:\\Users\\Public\\test.txt");
   }
 
 }
