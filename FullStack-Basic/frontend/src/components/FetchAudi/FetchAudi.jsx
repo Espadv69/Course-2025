fc8f12ebff177342beb4bc48e8db34ef0a540db3
Espadv69 <aitorjob69@gmail.com>
2025-03-14 11:33:40 +0100

    Improve error handling and render Audi list in FetchAudi component

--- FullStack-Basic/frontend/src/components/FetchAudi/FetchAudi.jsx
+++ FullStack-Basic/frontend/src/components/FetchAudi/FetchAudi.jsx
@@ -10,7 +10,7 @@ const FetchAudi = () => {
     const fetchAudis = async () => {
       try {
         const reponse = await fetch(LOCAL_URL)
-        if (!reponse.ok) return console.error('Error:', reponse.statusText)
+        if (!reponse.ok) throw new Error('Server error')
 
         const data = await reponse.json()
         setAudis(data)
@@ -21,6 +21,25 @@ const FetchAudi = () => {
 
     fetchAudis()
   }, [])
+
+  return (
+    <section className="container__audis">
+      <h2>Audis API</h2>
+
+      <ul className="ul__audis">
+        {audis.map((audi) => (
+          <li key={audi.id}>
+            <h3>{audi.name}</h3>
+            <p>{audi.description}</p>
+            <strong>
+              {audi.price}€ <span>{audi.year}</span>
+            </strong>
+            <img src={audi.img} alt={audi.name} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
 }
 
 export default FetchAudi
