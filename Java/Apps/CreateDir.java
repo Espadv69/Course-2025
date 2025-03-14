74ba25b6e4f8c3f3bb7c11c02610c79e6e04f13f
Espadv69 <aitorjob69@gmail.com>
2025-03-14 09:47:22 +0100

    Add directory creation logic to CreateDir class

--- Java/Apps/CreateDir.java
+++ Java/Apps/CreateDir.java
@@ -1,9 +1,19 @@
 package Apps;
 
+import java.io.File;
+
 public class CreateDir {
 
   public static void main(String[] args) {
+    String dirName = "Apps/MyDir";
+    File dir = new File(dirName);
 
+    if (dir.exists()) {
+      System.out.println("El directorio " + dirName + " ya existe.");
+    } else {
+      dir.mkdir();
+      System.out.println("El directorio " + dirName + " ha sido creado.");
+    }
   }
 
 }
