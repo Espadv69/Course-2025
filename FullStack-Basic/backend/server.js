3c0b02ea88bc99771e938abe66bcc702f78699c8
Espadv69 <aitorjob69@gmail.com>
2025-03-14 11:22:31 +0100

    Refactor server routes to use constants for home and audi API endpoints

--- FullStack-Basic/backend/server.js
+++ FullStack-Basic/backend/server.js
@@ -2,17 +2,20 @@ const express = require('express')
 const cors = require('cors')
 const audi = require('./data/data.js')
 
+const HOME_ROUTE = require('./utils/routes.js').HOME_ROUTE
+const AUDI_URL = require('./utils/routes.js').AUDI_URL
+
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 const PORT = 5000
 
-app.get('/', async (req, res) => {
+app.get(HOME_ROUTE, async (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/api/audis', async (req, res) => {
+app.get(AUDI_URL, async (req, res) => {
   res.json(audi)
 })
 
