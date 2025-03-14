1b9c84ed29e7669c5b72560f31deaf2f45f9b1cf
Espadv69 <aitorjob69@gmail.com>
2025-03-14 10:39:40 +0100

    Enhance DeleteDir class to check for directory existence before deletion

--- Java/Apps/DeleteDir.java
+++ Java/Apps/DeleteDir.java
@@ -1,9 +1,18 @@
 package Apps;
 
+import java.io.File;
+
 public class DeleteDir {
 
   public static void main(String[] args) {
+    File dir = new File("C:\\Users\\Mañana\\Documents\\Course-2025\\aaa");
 
+    if (dir.exists()) {
+      System.out.println("Deleting " + dir);
+      dir.delete();
+    } else {
+      System.out.println("Directory does not exist");
+    }
   }
 
 }
