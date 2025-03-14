9566d1871c31c043f8da545b0809fda5d8a1bb4f
Espadv69 <aitorjob69@gmail.com>
2025-03-14 12:36:57 +0100

    Add file writing functionality to CreateWithTxt class

--- Java/Apps/CreateWithTxt.java
+++ Java/Apps/CreateWithTxt.java
@@ -1,9 +1,20 @@
 package Apps;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class CreateWithTxt {
 
   public static void main(String[] args) {
+    String path = "C:\\Users\\Mañana\\Documents\\Course-2025\\Java\\Apps\\test.md";
+    String content = "## Hello World!\nThis is a test file.\n";
 
+    try {
+      Files.write(Paths.get(path), content.getBytes());
+      System.out.println("File created successfully.");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
 }
