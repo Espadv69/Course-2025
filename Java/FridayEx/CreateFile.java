3bbc9f78853704160cda6af47b8b2e0f6a8a332f
Espadv69 <aitorjob69@gmail.com>
2025-03-14 13:43:21 +0100

    Add file creation logic in CreateFile class

--- Java/FridayEx/CreateFile.java
+++ Java/FridayEx/CreateFile.java
@@ -1,9 +1,24 @@
 package FridayEx;
 
+import java.io.File;
+
 public class CreateFile {
 
   public static void main(String[] args) {
+    String path = "C:\\Users\\Public\\test.txt";
+    File file = new File(path);
 
+    if (file.exists()) {
+      System.out.println("El archivo ya existe.");
+      return;
+    } else {
+      try {
+        file.createNewFile();
+        System.out.println("El archivo ha sido creado.");
+      } catch (Exception e) {
+        e.getStackTrace();
+      }
+    }
   }
 
 }
