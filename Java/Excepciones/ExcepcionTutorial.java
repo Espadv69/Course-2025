0c92b738adec29e450f9df17491d5d2e1c192d98
Espadv69 <aitorjob69@gmail.com>
2025-03-18 10:05:21 +0100

    Add Scanner import and instantiate in ExcepcionTutorial main method

--- Java/Excepciones/ExcepcionTutorial.java
+++ Java/Excepciones/ExcepcionTutorial.java
@@ -1,9 +1,13 @@
 package Excepciones;
 
+import java.util.Scanner;
+
 public class ExcepcionTutorial {
 
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
+    sc.close();
   }
 
 }
