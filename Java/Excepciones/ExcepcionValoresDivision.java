539b9e1fd1457743d9ed641a00b9f693d1026a84
Espadv69 <aitorjob69@gmail.com>
2025-03-18 10:37:15 +0100

    Add input handling and exception management in ExcepcionValoresDivision class

--- Java/Excepciones/ExcepcionValoresDivision.java
+++ Java/Excepciones/ExcepcionValoresDivision.java
@@ -1,11 +1,31 @@
 package Excepciones;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExcepcionValoresDivision {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+
+    try {
+
+      System.out.println("Introduce el dividendo: ");
+      int dividendo = sc.nextInt();
+
+      System.out.println("Introduce el divisor: ");
+      int divisor = sc.nextInt();
+
+      int resultado = dividendo / divisor;
+      System.out.println("El resultado de la división es: " + resultado);
+
+    } catch (InputMismatchException exception) {
+      System.err.println("InputMismatchError: " + exception.getMessage());
+    } catch (ArithmeticException exception) {
+      System.err.println("ArithmeticError: " + exception.getMessage());
+    } finally {
+      System.out.println("Operación finalizada.");
+    }
 
     sc.close();
   }
