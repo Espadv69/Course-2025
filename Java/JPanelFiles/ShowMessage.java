0d3a00f81a910317a831ac8e7ce38d5ec75d5aeb
Espadv69 <aitorjob69@gmail.com>
2025-03-18 10:26:27 +0100

    Add Dialog_input and ShowMessage classes for user interaction

---
+++ Java/JPanelFiles/ShowMessage.java
@@ -0,0 +1,11 @@
+package JPanelFiles;
+
+import javax.swing.JOptionPane;
+
+public class ShowMessage {
+  
+  public static void main(String[] args) {
+    JOptionPane.showMessageDialog(null, "Hola Mundo");
+  }
+
+}
