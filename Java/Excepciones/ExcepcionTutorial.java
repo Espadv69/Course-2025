5b1f7d8be32dd4e3b15605d3ad61309981f815bf
Espadv69 <aitorjob69@gmail.com>
2025-03-18 10:07:20 +0100

    Add functionality to calculate and display the square of an integer input in ExcepcionTutorial

--- Java/Excepciones/ExcepcionTutorial.java
+++ Java/Excepciones/ExcepcionTutorial.java
@@ -7,6 +7,12 @@ public class ExcepcionTutorial {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
+    System.out.println("Ingrese un valor entero: ");
+    int valor = sc.nextInt();
+
+    int cuadrado = valor * valor;
+    System.out.println("El cuadrado de " + valor + " es " + cuadrado);
+
     sc.close();
   }
 
