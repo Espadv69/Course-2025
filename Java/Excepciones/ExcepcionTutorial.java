54974c8f9efb49525c8939505fe88a9a2a2c8fe2
Espadv69 <aitorjob69@gmail.com>
2025-03-18 10:10:03 +0100

    Add error handling for integer input in ExcepcionTutorial

--- Java/Excepciones/ExcepcionTutorial.java
+++ Java/Excepciones/ExcepcionTutorial.java
@@ -7,11 +7,16 @@ public class ExcepcionTutorial {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    System.out.println("Ingrese un valor entero: ");
-    int valor = sc.nextInt();
+    try {
+      System.out.println("Ingrese un valor entero: ");
+      int valor = sc.nextInt();
 
-    int cuadrado = valor * valor;
-    System.out.println("El cuadrado de " + valor + " es " + cuadrado);
+      int cuadrado = valor * valor;
+      System.out.println("El cuadrado de " + valor + " es " + cuadrado);
+    } catch (Exception e) {
+      System.err.println("Error: " + e.getMessage());
+      System.out.println("Por favor, ingrese un valor entero.");
+    }
 
     sc.close();
   }
