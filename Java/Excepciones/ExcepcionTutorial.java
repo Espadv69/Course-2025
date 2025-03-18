8b3c862a69296247eca8b0a2b836ddf2c51434fb
Espadv69 <aitorjob69@gmail.com>
2025-03-18 10:23:51 +0100

    Implement loop for repeated integer input until valid in ExcepcionTutorial

--- Java/Excepciones/ExcepcionTutorial.java
+++ Java/Excepciones/ExcepcionTutorial.java
@@ -8,16 +8,23 @@ public class ExcepcionTutorial {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    try {
-      System.out.println("Ingrese un valor entero: ");
-      int valor = sc.nextInt();
+    boolean continuar;
 
-      int cuadrado = valor * valor;
-      System.out.println("El cuadrado de " + valor + " es " + cuadrado);
-    } catch (InputMismatchException ex) {
-      System.err.println("Error: " + ex.getMessage());
-      System.out.println("Por favor, ingrese un valor entero.");
-    }
+    do {
+      try {
+        continuar = false;
+        System.out.println("Ingrese un valor entero: ");
+        int valor = sc.nextInt();
+
+        int cuadrado = valor * valor;
+        System.out.println("El cuadrado de " + valor + " es " + cuadrado);
+      } catch (InputMismatchException ex) {
+        System.err.println("Error: " + ex.getMessage());
+        System.out.println("Por favor, ingrese un valor entero.");
+        sc.nextLine();
+        continuar = true;
+      }
+    } while (continuar);
 
     sc.close();
   }
